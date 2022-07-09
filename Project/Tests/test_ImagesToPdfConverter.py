c1c9b84f98115dd5b4435c11dfdcfe358f0b943f
p.dell <y#CvXewPPnw!ej"Ikh64w"GQi#j3"/\`biMD!OLMj9TrQ4Zo+1>
2022-07-10 00:36:13 +0200

    added cleanup after test_single_page_pdf_without_ocr

--- Project/Tests/test_ImagesToPdfConverter.py
+++ Project/Tests/test_ImagesToPdfConverter.py
@@ -29,13 +29,15 @@ class TestPdfToImagesConverter(TestCase):
         self.assertEqual(test_stout.getvalue(), "")  # TODO maybe change condition
 
     def test_single_page_pdf_without_ocr(self):
+        if os.path.exists(self.dest_file):
+            os.remove(self.dest_file)
         pdf_converter = PdfToImageConverter(r"./TestData/singlePagePdf.pdf",
                                             self.working_dir, 1)
         pdf_converter.convert()
         image_converter = ImagesToPdfConverter(self.working_dir, self.dest_file)
         image_converter.convert()
         self.assertTrue(os.path.exists(self.dest_file))
-        self.assertFalse(os.path.isdir(self.working_dir))
+        OsUtility.clean_up_folder(self.working_dir)
 
     def test_single_page_pdf_wit_ocr(self):
         self.fail("not implemented yet")
