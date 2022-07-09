cb479766f63a1971d1e1da207a5333ede144af25
p.dell <y#CvXewPPnw!ej"Ikh64w"GQi#j3"/\`biMD!OLMj9TrQ4Zo+1>
2022-07-10 00:15:31 +0200

    Removed old packages

--- Converter/ConvertException.py
+++
@@ -1,8 +0,0 @@
-RED = "\033[0;31m"
-END = "\033[0m"
-
-
-class ConvertException(Exception):
-    def __init__(self, stage="Converting"):
-        super().__init__(RED + "[!] Converting Failed during " + stage + " stage." + END)
-
