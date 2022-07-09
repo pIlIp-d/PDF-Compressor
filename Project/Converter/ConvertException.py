814728fb6225918c9f04601d4c601e718a113f6d
p.dell <y#CvXewPPnw!ej"Ikh64w"GQi#j3"/\`biMD!OLMj9TrQ4Zo+1>
2022-07-10 00:10:54 +0200

    changed to f string

--- Project/Converter/ConvertException.py
+++ Project/Converter/ConvertException.py
@@ -3,5 +3,5 @@ from Project.Utility.ConsoleUtility import ConsoleUtility
 
 class ConvertException(Exception):
     def __init__(self, stage="Converting"):
-        super().__init__(ConsoleUtility.get_error_string("[!] Converting Failed during " + stage + " stage."))
+        super().__init__(ConsoleUtility.get_error_string(f"[!] Converting Error during {stage} stage."))
 
