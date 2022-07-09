e1c39a3d8578f86ab8be580b7436a8d3b23db873
p.dell <y#CvXewPPnw!ej"Ikh64w"GQi#j3"/\`biMD!OLMj9TrQ4Zo+1>
2022-07-10 00:13:43 +0200

    refactored and added pillow instead if PIL for better support

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-  name = "PDF Compressor",
-  description = "Compresses PDFs, made for GoodNotes",
-  url = "https://github.com/pIlIp-d/PDF-Compressor",
-  author = "pIlIp-d",
-  license = "MIT",
-  python_requires='>=3.6',
-  platforms = ["any"],
-  packages = find_packages(),
-  install_requires = ["os","argparse","shutil","subprocess","multiprocessing","PIL","PyMuPdf","img2pdf","pytesseract"],
-  include_package_data = True,
-  classifiers = [
+  name="PDF Compressor",
+  description="Compresses PDFs, made for GoodNotes",
+  url="https://github.com/pIlIp-d/PDF-Compressor",
+  author="pIlIp-d",
+  license="MIT",
+  python_requires='>=3.10',
+  platforms=["any"],
+  packages=find_packages(),
+  install_requires=["pillow", "PyMuPdf", "img2pdf", "pytesseract"],
+  include_package_data=True,
+  classifiers=[
     "License :: MIT",
     "Topic :: Software Development :: Libraries :: Python Module",
     "Topic :: Utilities :: Compression",
