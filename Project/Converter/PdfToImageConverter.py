d0defb5b373df28b44838814cf90bbc4006e7748
p.dell <y#CvXewPPnw!ej"Ikh64w"GQi#j3"/\`biMD!OLMj9TrQ4Zo+1>
2022-07-10 00:09:30 +0200

    improved tesseract import Logic

---
+++ Project/Converter/PdfToImageConverter.py
@@ -0,0 +1,25 @@
+from Project.Converter.Converter import *
+from Project.Utility.OsUtility import OsUtility
+
+import os
+
+# package name PyMuPdf
+import fitz
+
+
+class PdfToImageConverter(Converter):
+    def __init__(self, origin_path, dest_path, mode=5):
+        super().__init__(origin_path, dest_path)
+        self.mode = mode
+
+    def convert(self):
+        print(self.dest_path)
+        OsUtility.create_folder_if_not_exist(self.dest_path)
+        print("--splitting pdf into images--")
+        # open pdf and split it into rgb-pixelmaps -> png
+        doc = fitz.open(self.origin_path)
+        for page in doc:
+            print("** - {:.2f}%".format(100 * page.number / len(doc)))
+            pix = page.get_pixmap(matrix=fitz.Matrix(self.mode, self.mode))
+            pix.save(os.path.join(self.dest_path, 'page_%i.png' % page.number))
+        print("** - 100.00%")
