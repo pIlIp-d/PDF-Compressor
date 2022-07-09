34488ff52c50b6f47e24d4aa027581c2ee3d3c3f
p.dell <y#CvXewPPnw!ej"Ikh64w"GQi#j3"/\`biMD!OLMj9TrQ4Zo+1>
2022-07-09 21:58:01 +0200

    Refactored to OOP, added Test-Stubs

---
+++ Tests/test_OsUtility.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+import Utility.OsUtility as OsUtility
+
+
+class TestOsUtility(TestCase):
+    def test_get_file_list(self):
+        self.fail("Not Implemented Yet")
+
+    def test_clean_up_folder(self):
+        self.fail("Not Implemented Yet")
+
+    def test_create_folder_if_not_exist(self):
+        self.fail("Not Implemented Yet")
+
+    def test_get_filename(self):
+        self.fail("Not Implemented Yet")
---
+++ Tests/test_PdfToImagesConverter.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+
+class TestPdfToImagesConverter(TestCase):
+
+    def test_class_init(self):
+        self.fail("Not Implemented Yet")
+
+    def test_single_page_pdf_convert(self):
+        self.fail("Not Implemented Yet")
+
+    def test_multiple_page_pdf_convert(self):
+        self.fail("Not Implemented Yet")
+
+    def test_wrong_format_pdf(self):
+        self.fail("Not Implemented Yet")
+
+    def test_mode_is_0(self):
+        self.fail("Not Implemented Yet")
+
+    def test_mode_is_10(self):
+        self.fail("Not Implemented Yet")
+
+    def test_mode_is_5(self):
+        self.fail("Not Implemented Yet")
+
+# TODO different filename tests
