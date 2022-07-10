233a40a5401db70858ed8bfde0530a6fab39cf34
p.dell <y#CvXewPPnw!ej"Ikh64w"GQi#j3"/\`biMD!OLMj9TrQ4Zo+1>
2022-07-10 23:27:29 +0200

    Testing: documentation TestTables TODO Test for OsUtility.py

---
+++ Project/Tests/test_ConsolePDFCompressor.py
@@ -0,0 +1,35 @@
+import os.path
+import shutil
+import subprocess
+import sys
+from io import StringIO
+from unittest import TestCase
+
+from Project.Utility.OsUtility import OsUtility
+
+
+class TestConsolePDFCompressor(TestCase):
+    def remove_if_not_exists(self, file_path):
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+    def test_compress_single_file(self):
+        result_path = './TestData/multiPageTestData_compressed.pdf'
+        self.remove_if_not_exists(result_path)
+        subprocess.call(["python3", os.path.abspath('../PDFCompressor.py'),
+                         "-p", os.path.abspath('./TestData/multiPageTestData.pdf')])
+        self.assertTrue(os.path.exists(result_path))
+
+    def test_compress_single_file_with_output_path(self):
+        result_path = './TestData/new-min.pdf'
+        self.remove_if_not_exists(result_path)
+        OsUtility.clean_up_folder(os.path.abspath("./TestData/multiPageTestData_tmp"))
+
+        subprocess.call(["python3", os.path.abspath('../PDFCompressor.py'),
+                         "-p", os.path.abspath('./TestData/multiPageTestData.pdf'),
+                         "-o", os.path.abspath(result_path), "-m", "9"])
+        file_exists = os.path.exists(result_path)
+        os.remove(result_path)
+        self.assertTrue(file_exists)
+
+    # TODO relative paths
