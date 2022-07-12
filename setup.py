3281e628ea926b5c2ffdb7ce9015130873310c25
p.dell <y#CvXewPPnw!ej"Ikh64w"GQi#j3"/\`biMD!OLMj9TrQ4Zo+1>
2022-07-12 23:13:08 +0200

    format changed and requirements were added

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
-from setuptools import setup
 from setuptools import find_packages
+from setuptools import setup
 
 setup(
-  name="pdfcompressor",
-  description="Compresses PDFs extremely.",
-  url="https://github.com/pIlIp-d/PDF-Compressor",
-  author="pIlIp-d",
-  license="MIT",
-  python_requires='>=3.10',
-  platforms=["any"],
-  packages=find_packages(),
-  install_requires=["pillow", "PyMuPdf", "img2pdf", "pandas", "pytesseract"],
-  include_package_data=True,
-  classifiers=[
-    "License :: MIT",
-    "Topic :: Software Development :: Libraries :: Python Module",
-    "Topic :: Utilities :: Compression",
-    "Programming Language :: Python :: 3"
-  ]
+    name="pdfcompressor",
+    description="Compresses PDFs extremely.",
+    url="https://github.com/pIlIp-d/PDF-Compressor",
+    author="pIlIp-d",
+    license="MIT",
+    python_requires='>=3.10',
+    platforms=["any"],
+    packages=find_packages(),
+    install_requires=[
+        "pillow",
+        "PyMuPdf",
+        "img2pdf",
+        "pytesseract",
+        "jsons"
+    ],
+    include_package_data=True,
+    classifiers=[
+        "License :: MIT",
+        "Topic :: Software Development :: Libraries :: Python Module",
+        "Topic :: Utilities :: Compression",
+        "Programming Language :: Python :: 3"
+    ]
 )
