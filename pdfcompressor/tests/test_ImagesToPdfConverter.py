e44ada96e4368c2e24cd0c23430d3dda573f2f78
p.dell <y#CvXewPPnw!ej"Ikh64w"GQi#j3"/\`biMD!OLMj9TrQ4Zo+1>
2022-07-13 23:11:34 +0200

    Testing: removed old un-functional tests

--- pdfcompressor/tests/test_ImagesToPdfConverter.py
+++ pdfcompressor/tests/test_ImagesToPdfConverter.py
@@ -1,60 +1,8 @@
 import os.path
-import sys
-from io import StringIO
 from unittest import TestCase
-
-from pdfcompressor.compressor.converter.images_to_pdf_converter import ImagesToPdfConverter
-from pdfcompressor.compressor.converter.pdf_to_image_converter import PdfToImageConverter
-from pdfcompressor.utility.console_utility import ConsoleUtility
-from pdfcompressor.utility.os_utility import OsUtility
 
 
 class TestPdfToImagesConverter(TestCase):
     working_dir: str = os.path.abspath(r"./CurrentWorkingPathTest/")
     dest_file: str = os.path.abspath(r"TestData/TestFolder/result.pdf")
     tesseract_path: str = r"/usr/bin/tesseract"
-    # r"/home/user/Downloads/tesseract-ocr-w64-setup-v5.2.0.20220708.exe"
-
-    def test_class_init_simple(self):
-        ImagesToPdfConverter("originFile.pdf", "destinationFile.pdf")
-
-    def test_class_init_with_pytesseract_path(self):
-        ImagesToPdfConverter("originFile.pdf", "destinationFile.pdf", self.tesseract_path)
-
-    def test_class_init_without_force_ocr(self):
-        ImagesToPdfConverter("originFile.pdf", "destinationFile.pdf", self.tesseract_path, False)
-
-    def test_class_init_with_force_ocr(self):
-        test_stout = StringIO()
-        sys.stdout = test_stout
-        ImagesToPdfConverter("originFile.pdf", "destinationFile.pdf", self.tesseract_path, True)
-
-        # check that  no error occurred
-        self.assertFalse(test_stout.getvalue().__contains__(ConsoleUtility.RED))
-
-    def test_single_page_pdf_without_ocr(self):
-        if os.path.exists(self.dest_file):
-            os.remove(self.dest_file)
-        pdf_converter = PdfToImageConverter(r"TestData/TestFolder/singlePagePdf.pdf",
-                                            self.working_dir, 1)
-        pdf_converter.convert()
-        image_converter = ImagesToPdfConverter(self.working_dir, self.dest_file, self.tesseract_path, False)
-        image_converter.convert()
-        self.assertTrue(os.path.exists(self.dest_file))
-        OsUtility.clean_up_folder(self.working_dir)
-
-    def test_single_page_pdf_wit_ocr(self):
-        test_stout = StringIO()
-        sys.stdout = test_stout
-
-        if os.path.exists(self.dest_file):
-            os.remove(self.dest_file)
-        pdf_converter = PdfToImageConverter(r"TestData/TestFolder/singlePagePdf.pdf",
-                                            self.working_dir, 1)
-        pdf_converter.convert()
-        image_converter = ImagesToPdfConverter(self.working_dir, self.dest_file, self.tesseract_path, True)
-        image_converter.convert()
-        self.assertTrue(os.path.exists(self.dest_file))
-        OsUtility.clean_up_folder(self.working_dir)
-        # check that  no error occurred
-        self.assertFalse(test_stout.getvalue().__contains__(ConsoleUtility.RED))
--- pdfcompressor/tests/test_PdfToImagesConverter.py
+++ pdfcompressor/tests/test_PdfToImagesConverter.py
@@ -1,78 +1,7 @@
 import os.path
-import shutil
 from unittest import TestCase
-
-from pdfcompressor.compressor.converter.pdf_to_image_converter import PdfToImageConverter
-from pdfcompressor.utility.os_utility import OsUtility
 
 
 class TestPdfToImagesConverter(TestCase):
     working_dir = os.path.abspath(r"./CurrentWorkingPathTest/")
 
-    def test_class_init(self):
-        # without mode
-        PdfToImageConverter("originFile.pdf", "destinationFile.pdf")
-        # with mode
-        PdfToImageConverter("originFile.pdf", "destinationFile.pdf", 5)
-
-    def test_single_page_pdf_convert(self):
-        pdf_converter = PdfToImageConverter(r"TestData/TestFolder/singlePagePdf.pdf",
-                                            self.working_dir, 5)
-        pdf_converter.convert()
-        self.assertTrue(os.path.exists(self.working_dir+"/page_0.png"))
-        self.assertFalse(os.path.exists(self.working_dir + "/page_1.png"))
-
-    def test_multiple_page_pdf_convert(self):
-        pdf_converter = PdfToImageConverter(r"TestData/TestFolder/multiPageTestData.pdf",
-                                            self.working_dir, 1)
-        pdf_converter.convert()
-        self.assertTrue(os.path.exists(self.working_dir + "/page_0.png"))
-        self.assertTrue(os.path.exists(self.working_dir + "/page_1.png"))
-        self.assertFalse(os.path.exists(self.working_dir + "/page_2.png"))
-
-    def test_wrong_format_pdf(self):
-        # TODO expected exception
-        pdf_converter = PdfToImageConverter(r"TestData/TestFolder/multiPageTestData.pdf",
-                                            self.working_dir, 1)
-        pdf_converter.convert()
-        self.assertTrue(os.path.exists(self.working_dir + "/page_0.png"))
-        self.assertTrue(os.path.exists(self.working_dir + "/page_1.png"))
-        self.assertFalse(os.path.exists(self.working_dir + "/page_2.png"))
-
-    def test_mode_is_min_value(self):
-        pdf_converter = PdfToImageConverter(r"TestData/TestFolder/multiPageTestData.pdf",
-                                            self.working_dir, 0)
-        self.assertRaises(RuntimeError, pdf_converter.convert)
-
-    def test_mode_is_max_value(self):
-        pdf_converter = PdfToImageConverter(r"TestData/TestFolder/multiPageTestData.pdf",
-                                            self.working_dir, 10)
-        pdf_converter.convert()
-        self.assertTrue(os.path.exists(self.working_dir + "/page_0.png"))
-        self.assertTrue(os.path.exists(self.working_dir + "/page_1.png"))
-        self.assertFalse(os.path.exists(self.working_dir + "/page_2.png"))
-
-    def test_mode_is_negative_value(self):
-        pdf_converter = PdfToImageConverter(r"TestData/TestFolder/multiPageTestData.pdf",
-                                            self.working_dir, -1)
-        pdf_converter.convert()
-        self.assertTrue(os.path.exists(self.working_dir + "/page_0.png"))
-        self.assertTrue(os.path.exists(self.working_dir + "/page_1.png"))
-        self.assertFalse(os.path.exists(self.working_dir + "/page_2.png"))
-
-    def test_mode_is_too_large_value(self):
-        pdf_converter = PdfToImageConverter(r"TestData/TestFolder/multiPageTestData.pdf",
-                                            self.working_dir, 11)
-        pdf_converter.convert()
-        self.assertTrue(os.path.exists(self.working_dir + "/page_0.png"))
-        self.assertTrue(os.path.exists(self.working_dir + "/page_1.png"))
-        self.assertFalse(os.path.exists(self.working_dir + "/page_2.png"))
-
-    def setUp(self):
-        OsUtility.clean_up_folder(self.working_dir)
-
-    def tearDown(self):
-        if os.path.isdir(self.working_dir):
-            shutil.rmtree(self.working_dir)
-
-# TODO different filename tests
