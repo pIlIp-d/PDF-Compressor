86db84b022cfbd10b3efa56fc34cf19573deaaa7
p.dell <y#CvXewPPnw!ej"Ikh64w"GQi#j3"/\`biMD!OLMj9TrQ4Zo+1>
2022-07-14 10:15:18 +0200

    added error messages dont quit config.py -> use it even if you dont have every dependency

--- config.py
+++ config.py
@@ -3,45 +3,46 @@ import os
 from pdfcompressor.utility.console_utility import ConsoleUtility
 
 
-def check_existence(path, error_message):
+def check_existence(path, error_message) -> str:
     if not os.path.exists(path):
         ConsoleUtility.print(
             ConsoleUtility.get_error_string(error_message)
             + " If you're running it with sudo try it without."
         )
-        exit()
+        return path
+    else:
+        return "not found"
 
 
-cpdfsqueeze_path = os.path.abspath(os.path.join(
-    os.path.dirname(__file__),
-    "pdfcompressor", "compressor", "compressor_lib", "cpdfsqueeze", "cpdfsqueeze.exe")
-)
-check_existence(
-    cpdfsqueeze_path,
+cpdfsqueeze_path = check_existence(
+    os.path.abspath(os.path.join(
+        os.path.dirname(__file__),
+        "pdfcompressor", "compressor", "compressor_lib", "cpdfsqueeze", "cpdfsqueeze.exe")
+    ),
     "cpdfsqueeze path not found. You need to install or download it. cpdfsqueeze.exe run by wine"
 )
 
 if os.name == "nt":
-    pngquant_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "compressor", "compressor_lib", "pngquant", "pngquant.exe")
-    advpng_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "compressor", "compressor_lib", "advpng", "advpng.exe")
-    tesseract_path = os.path.join(os.path.abspath(os.path.expanduser('~')), "AppData", "Local", "Programs", "Tesseract-OCR",
+    pngquant_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "compressor", "compressor_lib", "pngquant",
+                                 "pngquant.exe")
+    advpng_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "compressor", "compressor_lib", "advpng",
+                               "advpng.exe")
+    tesseract_path = os.path.join(os.path.abspath(os.path.expanduser('~')), "AppData", "Local", "Programs",
+                                  "Tesseract-OCR",
                                   "tesseract.exe")
 
 else:
-    pngquant_path = os.path.join("/", "usr", "bin", "pngquant")
-    advpng_path = os.path.join("/", "usr", "bin", "advpng")
-    tesseract_path = os.path.join(os.path.abspath(os.path.expanduser('~')), ".local", "bin", "pytesseract")
     # Linux: errors if paths weren't found
-    check_existence(
-        pngquant_path,
+    pngquant_path = check_existence(
+        os.path.join("/", "usr", "bin", "pngquant"),
         "Pngquant path not found. Install it with 'sudo apt install pngquant'."
     )
-    check_existence(
-        advpng_path,
+    advpng_path = check_existence(
+        os.path.join("/", "usr", "bin", "advpng"),
         "advpng path not found. Install it with 'sudo apt install advancecomp'."
     )
-    check_existence(
-        tesseract_path,
+    tesseract_path = check_existence(
+        os.path.join(os.path.abspath(os.path.expanduser('~')), ".local", "bin", "pytesseract"),
         "pytesseract path not found. Install it with 'sudo apt install "
         "pytesseract-ocr'. Additionally add language packs with f.e. "
         "'german/deutsch': 'sudo apt install pytesseract-ocr-deu'"
