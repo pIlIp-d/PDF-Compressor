e575c3b4af14a5f6c0ac05ae97a869b1da2658bb
p.dell <y#CvXewPPnw!ej"Ikh64w"GQi#j3"/\`biMD!OLMj9TrQ4Zo+1>
2022-07-14 11:18:45 +0200

    cleaned configuration file and added tessdata_prefix

--- config.py
+++ config.py
@@ -11,42 +11,43 @@ def check_existence(path, error_message) -> str:
         )
         return path
     else:
-        return "not found"
+        return ""  # Not Found
 
+
+compressor_lib_path = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), "pdfcompressor", "compressor", "compressor_lib"
+))
 
 cpdfsqueeze_path = check_existence(
-    os.path.abspath(os.path.join(
-        os.path.dirname(__file__),
-        "pdfcompressor", "compressor", "compressor_lib", "cpdfsqueeze", "cpdfsqueeze.exe")
-    ),
-    "cpdfsqueeze path not found. You need to install or download it. cpdfsqueeze.exe run by wine"
+    path=os.path.join(compressor_lib_path, "cpdfsqueeze", "cpdfsqueeze.exe"),
+    error_message="cpdfsqueeze path not found. You need to install or download it. cpdfsqueeze.exe run by wine"
 )
 
 if os.name == "nt":
-    pngquant_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "compressor", "compressor_lib", "pngquant",
-                                 "pngquant.exe")
-    advpng_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "compressor", "compressor_lib", "advpng",
-                               "advpng.exe")
+    # WINDOWS
+    pngquant_path = os.path.join(compressor_lib_path, "pngquant", "pngquant.exe")
+    advpng_path = os.path.join(compressor_lib_path, "pdfcompressor", "compressor", "compressor_lib", "advpng", "advpng.exe")
     tesseract_path = os.path.join(os.path.abspath(os.path.expanduser('~')), "AppData", "Local", "Programs",
-                                  "Tesseract-OCR",
-                                  "tesseract.exe")
+                                  "Tesseract-OCR", "tesseract.exe")
+    tessdata_prefix = f"--tessdata-dir '{os.path.join(os.path.expanduser('~'), 'AppData', 'Local', 'Programs', 'Tesseract-OCR', 'tessdata')}'"
 
 else:
-    # Linux: errors if paths weren't found
+    # LINUX
     pngquant_path = check_existence(
-        os.path.join("/", "usr", "bin", "pngquant"),
-        "Pngquant path not found. Install it with 'sudo apt install pngquant'."
+        path=os.path.join("/", "usr", "bin", "pngquant"),
+        error_message="Pngquant path not found. Install it with 'sudo apt install pngquant'."
     )
     advpng_path = check_existence(
-        os.path.join("/", "usr", "bin", "advpng"),
-        "advpng path not found. Install it with 'sudo apt install advancecomp'."
+        path=os.path.join("/", "usr", "bin", "advpng"),
+        error_message="advpng path not found. Install it with 'sudo apt install advancecomp'."
     )
     tesseract_path = check_existence(
-        os.path.join(os.path.abspath(os.path.expanduser('~')), ".local", "bin", "pytesseract"),
-        "pytesseract path not found. Install it with 'sudo apt install "
-        "pytesseract-ocr'. Additionally add language packs with f.e. "
-        "'german/deutsch': 'sudo apt install pytesseract-ocr-deu'"
+        path=os.path.join(os.path.abspath(os.path.expanduser('~')), ".local", "bin", "pytesseract"),
+        error_message="pytesseract path not found. Install it with 'sudo apt install "
+                      "pytesseract-ocr'. Additionally add language packs with f.e. "
+                      "'german/deutsch': 'sudo apt install pytesseract-ocr-deu'"
     )
+    tessdata_prefix = ""
 
 print("advpng, pngquant, cpdfsqueeze and tesseract were found and their paths were saved to config.json")
 with open("config.json", "w") as config_file:
@@ -55,6 +56,7 @@ with open("config.json", "w") as config_file:
     "advpng_path" : "{advpng_path}",
     "pngquant_path" : "{pngquant_path}",
     "cpdfsqueeze_path" : "{cpdfsqueeze_path}",
-    "tesseract_path" : "{tesseract_path}"
+    "tesseract_path" : "{tesseract_path}",
+    "tessdata_prefix" : "{tessdata_prefix}"
 ''' + "}"
     )
