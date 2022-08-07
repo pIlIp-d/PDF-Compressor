96d2ab578cde7f2b5f529bdc53e6bb4dc925e49d
pIlIp-d <62812264+pIlIp-d@users.noreply.github.com>
2022-08-07 04:59:49 +0200

    added superclass Processor to Compressor for implementation of pre- and postprocessing

--- pdfcompressor/compressor/compressor.py
+++ pdfcompressor/compressor/compressor.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 
+from ..processor import Processor
 
-class Compressor(ABC):
+
+class Compressor(ABC, Processor):
     @abstractmethod
     def compress(self, source_path: str, destination_path: str) -> None: pass
