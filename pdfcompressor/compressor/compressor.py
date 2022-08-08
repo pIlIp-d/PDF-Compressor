8068f881263b123006271232b200f0623373a7f3
pIlIp-d <62812264+pIlIp-d@users.noreply.github.com>
2022-08-08 06:01:42 +0200

    abstraction layer compression

--- pdfcompressor/compressor/compressor.py
+++ pdfcompressor/compressor/compressor.py
@@ -1,10 +1,20 @@
 from abc import ABC, abstractmethod
-from pdfcompressor.compressor.processor import Processor
+
+from pdfcompressor.processor.processor import Processor
 
 
 class Compressor(Processor, ABC):
-    def __init__(self, processor: Processor):
-        super().__init__(processor)
 
     @abstractmethod
     def compress(self, source_path: str, destination_path: str) -> None: pass
+
+    @abstractmethod
+    def compress_file(self, source_file: str, destination_file: str) -> None: pass
+
+    @abstractmethod
+    def compress_file_list(self, source_files: list, destination_files: list) -> None: pass
+
+    def compress_file_list_multi_threaded(self, source_files: list, destination_files: list) -> None:
+        args_list = [{"source_file": source, "destination_file": destination}
+                        for source, destination in zip(source_files, destination_files)]
+        self._custom_map_execute(self.compress_file, args_list)
