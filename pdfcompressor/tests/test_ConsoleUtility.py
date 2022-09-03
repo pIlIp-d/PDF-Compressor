c931ad0a0c26906732da2eddae372e618f9ff6ba
pilip-d <philipdell@gmx.de>
2022-09-03 18:27:52 +0200

    added compressed_value to tests

--- pdfcompressor/tests/test_ConsoleUtility.py
+++ pdfcompressor/tests/test_ConsoleUtility.py
@@ -59,45 +59,55 @@ class ConsoleUtilityTest(TestCase):
         ConsoleUtility.QUIET_MODE = False
         self.assertRaises(
             ValueError,
-            ConsoleUtility.print_stats, 0, 150
+            ConsoleUtility.print_stats, 0, 150, "File"
         )
 
     def test_print_stats_with_negative_orig(self):
         ConsoleUtility.QUIET_MODE = False
         self.assertRaises(
             ValueError,
-            ConsoleUtility.print_stats, -5, 150
+            ConsoleUtility.print_stats, -5, 150, "File"
         )
 
     def test_print_stats_orig_smaller_than_result(self):
         console_buffer = ConsoleUtilityTest.get_console_buffer()
         ConsoleUtility.QUIET_MODE = False
-        ConsoleUtility.print_stats(100, 150)
+        ConsoleUtility.print_stats(100, 150, "File")
         self.assertTrue(console_buffer.getvalue().__contains__("-50.0%"))
 
     def test_print_stats_orig_bigger_than_result(self):
         console_buffer = ConsoleUtilityTest.get_console_buffer()
         ConsoleUtility.QUIET_MODE = False
-        ConsoleUtility.print_stats(200, 150)
+        ConsoleUtility.print_stats(200, 150, "File")
         self.assertTrue(console_buffer.getvalue().__contains__("-25.0%"))
 
     def test_print_stats_orig_equal_to_result(self):
         console_buffer = ConsoleUtilityTest.get_console_buffer()
         ConsoleUtility.QUIET_MODE = False
-        ConsoleUtility.print_stats(150, 150)
+        ConsoleUtility.print_stats(150, 150, "File")
         self.assertTrue(console_buffer.getvalue().__contains__("-0.0%"))
 
     def test_print_stats_with_zero_as_result(self):
         console_buffer = ConsoleUtilityTest.get_console_buffer()
         ConsoleUtility.QUIET_MODE = False
-        ConsoleUtility.print_stats(150, 0)
+        ConsoleUtility.print_stats(150, 0, "File")
         self.assertTrue(console_buffer.getvalue().__contains__("-100.0%"))
 
-    # TODO tests for ConsoleUtility.print_stats() 'compressed_value' parameter
+    def test_print_stats_with_empty_string_as_compressed_value(self):
+        console_buffer = ConsoleUtilityTest.get_console_buffer()
+        ConsoleUtility.QUIET_MODE = False
+        ConsoleUtility.print_stats(150, 0, "")
+        self.assertTrue(console_buffer.getvalue().__contains__("  "))
+
+    def test_print_stats_normal_compressed_value(self):
+        console_buffer = ConsoleUtilityTest.get_console_buffer()
+        ConsoleUtility.QUIET_MODE = False
+        ConsoleUtility.print_stats(150, 0, "File")
+        self.assertTrue(console_buffer.getvalue().__contains__("File"))
 
     def test_print_stats_with_negative_result(self):
         ConsoleUtility.QUIET_MODE = False
         self.assertRaises(
             ValueError,
-            ConsoleUtility.print_stats, 150, -5
+            ConsoleUtility.print_stats, 150, -5, "File"
         )
