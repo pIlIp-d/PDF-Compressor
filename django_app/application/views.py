2a058f987c7131859766979fa3365f16bfd8a8c1
p.dell <y#CvXewPPnw!ej"Ikh64w"GQi#j3"/\`biMD!OLMj9TrQ4Zo+1>
2022-09-06 13:51:38 +0200

    migrated from Thore

--- django_app/application/views.py
+++ django_app/application/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView
+from django.http import HttpResponse, JsonResponse
+
+from .models import UploadedFile
+
+
 # Create your views here.
+def render_main_view(request):
+    uploaded_files = UploadedFile.objects.order_by('-date_of_upload')
+    context = {'uploaded_files': uploaded_files}
+    return render(request, 'application/main.html', context)
+
+
+def render_upload_view(request):
+    if request.method == 'POST':
+        current_file = request.FILES.get('file')
+        UploadedFile.objects.create(uploaded_file=current_file)
+        return HttpResponse('upload')
+    return JsonResponse({'post': 'false'})
+
+
+def render_form_submit_view(request):
+    return JsonResponse({'implemented': 'false'})
