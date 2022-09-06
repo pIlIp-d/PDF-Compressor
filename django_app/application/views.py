bee9fa157660fd4716906b5a0b7405f4ec3d2c47
p.dell <y#CvXewPPnw!ej"Ikh64w"GQi#j3"/\`biMD!OLMj9TrQ4Zo+1>
2022-09-06 17:35:02 +0200

    updated html, and implemented dynamic update of tesseract language

--- django_app/application/views.py
+++ django_app/application/views.py
@@ -1,3 +1,5 @@
+import os.path
+
 from django.shortcuts import render
 
 from django.views.generic import TemplateView
@@ -9,7 +11,20 @@ from .models import UploadedFile
 # Create your views here.
 def render_main_view(request):
     uploaded_files = UploadedFile.objects.order_by('-date_of_upload')
-    context = {'uploaded_files': uploaded_files}
+    context = {
+        'uploaded_files': uploaded_files,
+        "default_language": "eng",
+        "languages": [
+            {
+                "value": "eng",
+                "text": "English"
+            },
+            {
+                "value": "deu",
+                "text": "Deutsch"
+            }
+        ]
+    }
     return render(request, 'application/main.html', context)
 
 
@@ -23,4 +38,16 @@ def render_upload_view(request):
 
 
 def render_form_submit_view(request):
-    return JsonResponse({'implemented': 'false'})
+    # TODO convert to POST
+    if request.method == 'GET' and request.GET["submit"] == "Compress":
+        user_id = request.session['user_id']
+        # first show all files from the last request, that can be downloaded
+        # or if they are not ready, yet, show a wait thing
+        # underneath show all files that can be downloaded for this user_id (in defined time span
+        #
+        data_query = request.GET
+        user_id = request.session["user_id"]
+        file_folder_path = os.path.join(".", "media", "uploaded_files", f"user_{user_id}")
+
+        pass
+    return JsonResponse(request.GET)
