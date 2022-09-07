d91989ecdd89be85a522943ba0841d83353b7390
pilip-d <philipdell@gmx.de>
2022-09-07 10:29:52 +0200

    added api path array

--- django_app/application/urls.py
+++ django_app/application/urls.py
@@ -1,18 +1,21 @@
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
 app_name = 'application'
 
+rest_api_urlspatterns = [
+    path('processing_of_queue_is_finished/', views.processing_of_queue_is_finished, name='processing-of-queue-is-finished'),
+]
+
 urlpatterns = [
     path('', views.render_main_view, name='main-view'),
     path('pdf-compressor/', views.render_main_view, name='main-view'),
-    path('upload/', views.render_upload_view, name='upload-view'),
     path('form_submit/', views.render_form_submit_view, name='form-submit-view'),
-    # path('download_files/', views.render_download_files_view, name='download-files-view'),
-    # path('download_files/', views.render_download_files_view, name='download-files-view'),
+    path('api/', include(rest_api_urlspatterns)),
 ]
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
