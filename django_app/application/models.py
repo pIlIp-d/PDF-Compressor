5eb91f7e9bccd2a8cf2ed97ecebcba5dfc651f79
Thore <2120752@stud.hs-mannheim.de>
2022-09-07 14:58:27 +0200

    extensions werden nun erkannt und berücksichtigt, doppelte Dateien werden erkannt
    doppelt oder falsche Extension --> /media/invalid_files/

--- django_app/application/models.py
+++ django_app/application/models.py
@@ -1,12 +1,18 @@
 import os.path
+import pathlib
+
 from django.core.validators import FileExtensionValidator
 from django.db import models
 
 
 def get_directory_to_save_file_in(instance, filename: str) -> str:
     path = os.path.join("uploaded_files", f"user_{instance.user_id}", filename)
-    if os.path.isfile(os.path.join(".", "media", path)):
-        os.remove(os.path.join(".", "media", path))  # already a file with the same name --> override
+    extension_of_file = pathlib.Path(path).suffixes
+
+    # check presence of the file and the extension
+    if os.path.isfile(os.path.join(".", "media", path)) or extension_of_file[-1].lower() != '.pdf':
+        path = os.path.join("invalid_files", f"user_{instance.user_id}", filename)
+
     return path
 
 
@@ -14,8 +20,7 @@ class UploadedFile(models.Model):
     filename = models.TextField()
     user_id = models.CharField(max_length=64)
     finished = models.BooleanField(default=False)
-    uploaded_file = models.ImageField(upload_to=get_directory_to_save_file_in,
-                                      validators=[FileExtensionValidator(allowed_extensions=['.pdf'])])
+    uploaded_file = models.ImageField(upload_to=get_directory_to_save_file_in)
     date_of_upload = models.DateTimeField(auto_now_add=True)
     csrf_token = models.CharField(max_length=32)
 
@@ -29,4 +34,3 @@ class Meta:
         pass
 
     verbose_name_plural = 'Uploaded files'
-
