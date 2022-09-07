e36a261d83ce408e3b935389cf524b015139e57d
Thore <2120752@stud.hs-mannheim.de>
2022-09-07 03:20:16 +0200

    path-Erweiterungen auskommentiert (Probleme mit Fehlercode)
    app_name = 'application' --> im Falle von 2 urls.py geeignet um Bugs vorzubeugen

--- django_app/application/urls.py
+++ django_app/application/urls.py
@@ -1,15 +1,17 @@
-
 from django.urls import path
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+
+app_name = 'application'
 
 urlpatterns = [
     path('', views.render_main_view, name='main-view'),
     path('pdf-compressor/', views.render_main_view, name='main-view'),
     path('upload/', views.render_upload_view, name='upload-view'),
     path('form_submit/', views.render_form_submit_view, name='form-submit-view'),
-    path('download_files/', views.render_download_files_view, name='download-files-view'),
+    # path('download_files/', views.render_download_files_view, name='download-files-view'),
+    # path('download_files/', views.render_download_files_view, name='download-files-view'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
