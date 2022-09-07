8844515f66c25b7a6d5197e6c3b7905584dbd67c
Thore <2120752@stud.hs-mannheim.de>
2022-09-07 14:56:52 +0200

    Zeilenumbruch

--- django_app/application/urls.py
+++ django_app/application/urls.py
@@ -6,7 +6,8 @@ from . import views
 app_name = 'application'
 
 rest_api_urlspatterns = [
-    path('processing_of_queue_is_finished/', views.processing_of_queue_is_finished, name='processing-of-queue-is-finished'),
+    path('processing_of_queue_is_finished/', views.processing_of_queue_is_finished, name='processing-of-queue-is'
+                                                                                         '-finished'),
 ]
 
 urlpatterns = [
