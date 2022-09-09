f792b1f27d7d01600b5c165b2dc4fd1f5f0e00d2
pilip-d <philipdell@gmx.de>
2022-09-10 01:39:36 +0200

    moved validations to validators.py

--- django_app/webserver/models.py
+++ django_app/webserver/models.py
@@ -1,8 +1,8 @@
 import os
 import os.path
-import pathlib
 from django.db import models
-from jsons import ValidationError
+
+from django_app.webserver.validators import check_file_extension, check_file_size
 
 MEDIA_FOLDER_PATH = os.path.abspath(os.path.join("../../application", "media"))
 
@@ -11,19 +11,8 @@ def get_directory_for_file(user_id: str, csrf_token: str) -> str:
     return os.path.join("uploaded_files", f"user{user_id}", csrf_token[:10])
 
 
-def check_file_extension(path):
-    extension_of_file = pathlib.Path(path).suffixes[-1].lower()
-    if extension_of_file != '.pdf':
-        raise ValidationError("Invalid extension. Only PDF-File's are allowed.")
-
-
-def check_file_size(instance):
-    file_size = instance.uploaded_file.size
-    if file_size > UploadedFile.MAX_FILESIZE:
-        raise ValidationError("The maximum file size is reached.")
-
-
-# todo use request_queue id for file path, because it is getting too long (id from csrf_token) new Model=processing_request
+# todo use request_queue id for file path, because it is getting too long (id from csrf_token)
+#  new Model=processing_request
 def get_destination_directory(instance, filename: str) -> str:
     path = os.path.join(get_directory_for_file(instance.user_id, instance.csrf_token), filename)
     check_file_extension(path)
