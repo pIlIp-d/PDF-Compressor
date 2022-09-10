32fffda5e16d65477ce30dcc990b0374e0c79c77
pilip-d <philipdell@gmx.de>
2022-09-10 02:13:47 +0200

    moved db file

--- django_app/settings.py
+++ django_app/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'django_app' / 'db.sqlite3',
     }
 }
 
