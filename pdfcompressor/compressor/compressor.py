ee741d6dc62202fd9c9491ec6756720310cb08bc
pilip-d <philipdell@gmx.de>
2022-09-12 20:39:32 +0200

    added custom cpu count to multiprocessing call

--- pdfcompressor/compressor/compressor.py
+++ pdfcompressor/compressor/compressor.py
@@ -14,7 +14,7 @@ class Compressor(Processor, ABC):
     @abstractmethod
     def compress_file_list(self, source_files: list, destination_files: list) -> None: pass
 
-    def compress_file_list_multi_threaded(self, source_files: list, destination_files: list) -> None:
+    def compress_file_list_multi_threaded(self, source_files: list, destination_files: list, cpu_count: int) -> None:
         args_list = [{"source_file": source, "destination_file": destination}
                      for source, destination in zip(source_files, destination_files)]
-        self._custom_map_execute(self.compress_file, args_list)
+        self._custom_map_execute(self.compress_file, args_list, cpu_count)
