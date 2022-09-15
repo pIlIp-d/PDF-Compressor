f5af29f316b9249890227baf153a5c84a27fc8ac
pilip-d <philipdell@gmx.de>
2022-09-15 02:53:42 +0200

    refactored

--- django_app/webserver/urls.py
+++ django_app/webserver/urls.py
@@ -17,6 +17,5 @@ urlpatterns = [
     path('api/', include(api_urls)),
 ]
 
-
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
