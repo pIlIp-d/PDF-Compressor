5f8d8abe779104e09ee723d74cbf664daf00f05c
pilip-d <philipdell@gmx.de>
2022-09-16 22:22:40 +0200

    refactored ro separate class for task_scheduler

---
+++ django_app/webserver/custom_models/string_utility.py
@@ -0,0 +1,22 @@
+# no django imports allowed inside this file, because it is used by task_scheduler.py
+import os
+
+MEDIA_FOLDER_PATH = os.path.join(os.path.dirname(__file__), "..", "..", "media")
+
+
+class StringUtility:
+    @classmethod
+    def get_merged_destination_filename(cls, path_extra: str, request_id: str, datetime):
+        return f"{path_extra}_files_{request_id}_{cls.get_formatted_time(datetime)}"
+
+    @classmethod
+    def get_local_relative_path(cls, download_path):
+        return os.path.join(MEDIA_FOLDER_PATH, download_path)
+
+    @classmethod
+    def get_formatted_time(cls, t):
+        return t.strftime("%d.%m.%Y-%H:%M:%S")
+
+    @classmethod
+    def get_filename_with_ending(cls, file_path: str) -> str:
+        return file_path[len(os.path.dirname(file_path)) + 1:]
