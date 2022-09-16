8db71119de9bda55ad4644383fd2ebfdd1ed01b7
pilip-d <philipdell@gmx.de>
2022-09-16 22:24:20 +0200

    implemented tasks for task_scheduler

--- django_app/webserver/views.py
+++ django_app/webserver/views.py
@@ -1,19 +1,18 @@
-from apscheduler.triggers.date import DateTrigger
 from django.conf import settings
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
-from rest_apscheduler.scheduler import Scheduler
 
-from pdfcompressor.pdfcompressor import PDFCompressor
+from .custom_models.string_utility import StringUtility
 from .forms import PdfCompressorForm
-from .models import ProcessedFile, ProcessingFilesRequest, get_local_relative_path
-from .custom_models.process_stats_event_handler import ProcessStatsEventHandler
+from .models import ProcessedFile, ProcessingFilesRequest
 from ..api.views import wrong_method_error
+from ..task_scheduler.pdf_compression_task import PdfCompressionTask
 
 FORCE_SILENT_PROCESSING = False
 
 
 # TODO change to django-background-tasks lib
+# TODO tox, for basic functionality test after install (!= unitTesting)
 
 def render_main_view(request):
     allowed_file_endings = [".pdf", ".png"]
@@ -47,7 +46,6 @@ def render_download_view(request):
 
 
 # TODO POST value validating with django forms
-
 def start_pdf_compression_and_show_download_view(request):
     if request.method == 'POST':
         processing_request = ProcessingFilesRequest.get_or_create_new_request(
@@ -58,29 +56,30 @@ def start_pdf_compression_and_show_download_view(request):
 
         if processing_request.finished:
             return JsonResponse({"status": 429, "error": "You already send this request."}, status=429)
-        file_list = ProcessingFilesRequest.get_file_list_of_current_request(processing_request.id)
+        file_list = ProcessingFilesRequest.get_uploaded_file_list_of_current_request(processing_request.id)
         if len(file_list) < 1:
             return JsonResponse({"status": 412, "error": "No files were found for this request."}, status=412)
 
-        processed_files_list = []
-
         # add zip-file path
-        processed_zip_file = ProcessedFile.add_processed_file_by_id("", processing_request.id)
-        current_time = processed_zip_file.date_of_upload
-        # override processed_file_path
-        processed_zip_file.processed_file_path = processing_request.get_zip_destination_path(current_time)
-        processed_zip_file.save()
+        zip_file = ProcessedFile.add_processed_file_by_id("", processing_request.id)
+        current_time = zip_file.date_of_upload
 
-        destination_path = get_local_relative_path(processing_request.get_destination_dir())
+        # override processed_file_path
+        zip_file.processed_file_path = processing_request.get_merged_destination_path(current_time, ".zip")
+        zip_file.save()
+
+        processed_files_list = [zip_file]
+
+        destination_path = StringUtility.get_local_relative_path(processing_request.get_destination_dir())
         if request.POST.get("merge_pdfs") == "on":
             # add merge pdf-file path
             processed_file = ProcessedFile.add_processed_file_by_id(
-                processing_request.get_merged_destination_path(current_time, ".pdf"),
+                processing_request.get_merged_destination_path(zip_file.date_of_upload, ".pdf"),
                 processing_request.id
             )
             processed_files_list.append(processed_file)
             # change destination path to get merged result
-            destination_path = get_local_relative_path(processed_file.processed_file_path)
+            destination_path = StringUtility.get_local_relative_path(processed_file.processed_file_path)
         else:
             # add all results
             for file in file_list:
@@ -91,11 +90,11 @@ def start_pdf_compression_and_show_download_view(request):
                 processed_file.save()
                 processed_files_list.append(processed_file)
 
-        stats = list()
-        stats.append(ProcessStatsEventHandler(len(file_list), processed_files_list, processing_request))
-
-        pdf_compressor = PDFCompressor(
-            source_path=get_local_relative_path(processing_request.get_source_dir()),
+        PdfCompressionTask(
+            request_id=processing_request.id,
+            amount_of_input_files=len(file_list),
+            processed_file_paths=[file.processed_file_path for file in processed_files_list],
+            source_path=StringUtility.get_local_relative_path(processing_request.get_source_dir()),
             destination_path=destination_path,
             compression_mode=int(request.POST.get("compression_mode")),
             force_ocr=True if request.POST.get("ocr_mode") == "on" else False,
@@ -104,14 +103,8 @@ def start_pdf_compression_and_show_download_view(request):
             tesseract_language=request.POST.get("tesseract_language"),
             simple_and_lossless=True if request.POST.get("simple_and_lossless") == "on" else False,
             default_pdf_dpi=int(request.POST.get("default_pdf_dpi")),
-            event_handler=stats
         )
-        # start compression (async)
-        Scheduler.add_job(
-            pdf_compressor.compress,
-            trigger=DateTrigger(),
-            replace_existing=False
-        )
+
     else:
         return wrong_method_error("POST")
     return redirect("../download/")
