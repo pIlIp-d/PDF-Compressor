4537dd9a4fe7112617e32b0f53b9cf4217925449
pilip-d <philipdell@gmx.de>
2022-09-16 22:21:23 +0200

    added task_scheduler to perform processing without depending on the django main thread

---
+++ task_manager.py
@@ -0,0 +1,12 @@
+from time import sleep
+
+from django_app.task_scheduler.task_scheduler import TaskScheduler
+
+INTERVAL_TIME = 5
+
+if __name__ == "__main__":
+    task_scheduler = TaskScheduler()
+    while True:
+        if task_scheduler.check_for_unfinished_tasks():
+            task_scheduler.run_unfinished_tasks()
+        sleep(INTERVAL_TIME)
