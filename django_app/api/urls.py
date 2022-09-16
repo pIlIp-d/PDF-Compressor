814b497cb8ee3cb6530fdb6b21ef78b17bd3b37d
pilip-d <philipdell@gmx.de>
2022-09-16 22:23:58 +0200

    added api requests for interaction between task scheduler and django

--- django_app/api/urls.py
+++ django_app/api/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
          name='get-download-path-of-processed-file'),
     path('upload_file/', views.upload_file, name='upload-file'),
     path('remove_file/', views.remove_file, name='remove-file'),
+    path('started_request_processing/', views.started_request_processing, name='started_request_processing'),
+    path('finish_file/', views.finish_file, name='finish-file'),
+    path('finish_request/', views.finish_request, name='finish-request'),
 ]
