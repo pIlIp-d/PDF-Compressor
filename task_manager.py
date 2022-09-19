1727c147f12b494b2712cc22b087bb667a830b63
pilip-d <philipdell@gmx.de>
2022-09-19 20:39:43 +0200

    added TODO

--- task_manager.py
+++ task_manager.py
@@ -4,6 +4,7 @@ from django_app.task_scheduler.task_scheduler import TaskScheduler
 
 INTERVAL_TIME = 5
 
+# todo create socket that is called from task constructor instead if timer loop
 if __name__ == "__main__":
     task_scheduler = TaskScheduler()
     while True:
