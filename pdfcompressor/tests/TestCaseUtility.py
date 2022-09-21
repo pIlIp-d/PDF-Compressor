d71a133f269d3f1623b8061f6efe757bc1d0cdcf
pilip-d <philipdell@gmx.de>
2022-09-21 23:25:20 +0200

    added method for subprocess output

---
+++ pdfcompressor/tests/TestCaseUtility.py
@@ -0,0 +1,8 @@
+import subprocess
+from typing import Callable
+
+
+def run_subprocess_and_get_output(args: list) -> tuple[int, str, str]:
+    p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, err = p.communicate()
+    return p.returncode, str(output), str(err)
