e204327e5944588b26985231b837aaec27e467ec
pilip-d <philipdell@gmx.de>
2022-09-22 18:03:25 +0200

    removed unused class

--- pdfcompressor/compressor/converter/convert_exception.py
+++
@@ -1,7 +0,0 @@
-from pdfcompressor.utility.console_utility import ConsoleUtility
-
-
-class ConvertException(Exception):
-    def __init__(self, stage: str = "Converting"):
-        super().__init__(ConsoleUtility.get_error_string(f"[!] Converting Error during {stage} stage."))
-
