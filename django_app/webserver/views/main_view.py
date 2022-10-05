6868f5d32225961bc131b455beb01d7c64b33a1c
pilip-d <philipdell@gmx.de>
2022-10-05 22:51:49 +0200

    simplified view getters

--- django_app/webserver/views/main_view.py
+++ django_app/webserver/views/main_view.py
@@ -1,9 +1,12 @@
 from functools import reduce
 
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from django.views import View
 
-from django_app.webserver.forms.image_convert_form import ImageConvertForm, allowed_file_endings
+from django_app.webserver.forms import image_convert_form
+from django_app.webserver.forms.image_convert_form import ImageConvertForm
 from django_app.webserver.forms.pdf_compressor_form import PdfCompressorForm
 from django_app.webserver.forms.png_compressor_form import PngCompressorForm
 
@@ -17,47 +20,49 @@ def get_directory_for_html(request) -> str:
 
 
 def render_main_view(request):
-    allowed_file_endings = [".pdf"]  # no ',' allowed in file ending
-    form = PdfCompressorForm()
-    relative_dir = get_directory_for_html(request)
-    context = {
-        "dir": relative_dir,
-        "processing_action": "start_pdf_compression/",
-        "allowed_file_endings": ",".join(allowed_file_endings),
-        "form": form,
-        "user_id": request.session["user_id"],
-        "extra_scripts": ["pdf_compression.js"],
-        "form_html": loader.get_template('application/forms/pdf_compression_form.html').render({"form": form})
-    }
-    return render(request, 'application/main.html', context)
+    return get_processing_view(
+        request,
+        loader.get_template('application/forms/pdf_compression_form.html').render({"form": PdfCompressorForm()}),
+        "start_pdf_compression/",
+        [".pdf"],
+        ["pdf_compression.js"],
+    )
 
 
 def render_png_compression_view(request):
-    allowed_file_endings = [".png"]  # no ',' allowed in file ending
-    form = PngCompressorForm()
-    relative_dir = get_directory_for_html(request)
-    context = {
-        "dir": relative_dir,
-        "processing_action": "start_png_compression/",
-        "allowed_file_endings": ",".join(allowed_file_endings),
-        "form": form,
-        "user_id": request.session["user_id"],
-        "extra_scripts": [],
-        "form_html": loader.get_template('application/forms/png_compression_form.html').render({"form": form})
-    }
-    return render(request, 'application/main.html', context)
+    return get_processing_view(
+        request,
+        loader.get_template('application/forms/png_compression_form.html').render({"form": PngCompressorForm()}),
+        "start_png_compression/",
+        [".png"]
+    )
 
 
 def render_image_convert_view(request):
-    form = ImageConvertForm()
-    relative_dir = get_directory_for_html(request)
+    return get_processing_view(
+        request,
+        loader.get_template('application/forms/image_convert_form.html').render({"form": ImageConvertForm()}),
+        "start_png_compression/",  # TODO
+        image_convert_form.allowed_file_endings
+    )
+
+
+def get_processing_view(
+        request,
+        form_html: str,
+        processing_action: str,
+        allowed_file_endings: list,
+        extra_scripts=None
+):
+    if extra_scripts is None:
+        extra_scripts = list()
+
     context = {
-        "dir": relative_dir,
-        "processing_action": "start_png_compression/",
-        "allowed_file_endings": ",".join(allowed_file_endings),
+        "dir": get_directory_for_html(request),
+        "processing_action": processing_action,
+        "allowed_file_endings": ",".join(allowed_file_endings),  # no ',' allowed in file ending
         "user_id": request.session["user_id"],
-        "extra_scripts": [],
-        "form_html": loader.get_template('application/forms/image_convert_form.html').render({"form": form})
+        "extra_scripts": ",".join(extra_scripts),
+        "form_html": form_html
     }
-    print([(i, ending) for i, ending in zip(range(len(allowed_file_endings)), allowed_file_endings)])
     return render(request, 'application/main.html', context)
