2e706cfee655f1c7e7059df2ec5de779d28947c2
pilip-d <philipdell@gmx.de>
2022-10-08 23:21:44 +0200

    created extra EventHandler for zipping the result folder from ProcessStatsEventHandler

--- django_app/webserver/custom_models/process_stats_event_handler.py
+++ django_app/webserver/custom_models/process_stats_event_handler.py
@@ -1,11 +1,8 @@
-import os
-import shutil
 from time import sleep
 
 import jsons
 import requests
 
-from django_app.webserver.custom_models.string_utility import StringUtility
 from manage import ADDRESS, PORT, METHOD
 from pdfcompressor.utility.EventHandler import EventHandler
 from pdfcompressor.utility.os_utility import OsUtility
@@ -43,28 +40,9 @@ class ProcessStatsEventHandler(EventHandler):
                         str(file.processed_file_path)) + "_temp":
                     self.__make_request("/finish_file", {"processed_file_path": file.processed_file_path})
 
-    def _zip_result_folder(self):
-        result_file = self.__result_files[0]
-        destination_directory = os.path.dirname(result_file)
-        filename_without_file_ending = StringUtility.get_filename_with_ending(result_file)[:-len(".zip")]
-
-        # create zip-archive
-        compression_format = "zip"
-        shutil.make_archive(
-            filename_without_file_ending,
-            compression_format,
-            StringUtility.get_local_relative_path(destination_directory)
-        )
-
-        # move file into media folder
-        shutil.move(
-            os.path.join(".", filename_without_file_ending + ".zip"),
-            StringUtility.get_local_relative_path(os.path.join(destination_directory, filename_without_file_ending + ".zip"))
-        )
-        self.__make_request("/finish_request", {"request_id": str(self.__request_id)})
-
     def finished_all_files(self):
         if self.__is_merger:
+            # event: merged file is finished
             self.__make_request("/finish_file", {"processed_file_path": self.__result_files[1].processed_file_path})
-
-        self._zip_result_folder()
+        # event: all files are finished
+        self.__make_request("/finish_request", {"request_id": str(self.__request_id)})
