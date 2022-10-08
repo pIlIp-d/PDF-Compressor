be69775d00ecdbc2e34a65d94905c44e52771064
pilip-d <philipdell@gmx.de>
2022-10-08 22:04:56 +0200

    added request_id Middleware

--- django_app/webserver/middlewares.py
+++ django_app/webserver/middlewares.py
@@ -2,11 +2,17 @@ import string
 from django.utils.crypto import get_random_string
 
 
-class UserIdMiddleware:
+class CustomMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         # One-time configuration and initialization.
 
+    @staticmethod
+    def _get_new_id():
+        return get_random_string(50, allowed_chars=(string.ascii_letters + string.digits))
+
+
+class UserIdMiddleware(CustomMiddleware):
     def __call__(self, request):
         # create new user_id if not exists in session or cookie
         if 'user_id' in request.session:
@@ -14,7 +20,7 @@ class UserIdMiddleware:
         elif "user_id" in request.COOKIES:
             user_id = request.COOKIES["user_id"]
         else:
-            user_id = self.new_user_id()
+            user_id = self._get_new_id()
         # TODO expand to also allow User from django auth (-> multiple devices from a single user are possible)
 
         request.session['user_id'] = user_id
@@ -22,6 +28,11 @@ class UserIdMiddleware:
         response.set_cookie("user_id", user_id)
         return response
 
-    @staticmethod
-    def new_user_id() -> str:
-        return get_random_string(50, allowed_chars=(string.ascii_letters + string.digits))
+
+class RequestIdMiddleware(CustomMiddleware):
+    def __call__(self, request):
+        request_id = self._get_new_id()
+        request.session['request_id'] = request_id
+        response = self.get_response(request)
+        response.set_cookie("request_id", request_id)
+        return response
