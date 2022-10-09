cc407a6c322dd4468a95a0556961870351ef6a07
pilip-d <philipdell@gmx.de>
2022-10-09 23:31:17 +0200

    updated urls to new views

--- django_app/webserver/urls.py
+++ django_app/webserver/urls.py
@@ -10,14 +10,9 @@ app_name = 'webserver'
 
 urlpatterns = [
     path('', main_view.render_main_view, name='main-view'),
-    path('pdf-compressor/', main_view.render_main_view, name='main-view'),
-    path('png-compressor/', main_view.render_png_compression_view, name='main-view'),
-    path('image-convertor/', main_view.render_image_convert_view, name='render_image_convert_view'),
     path('download/', download_view.render_download_view, name='download_view'),
-    path('start_pdf_compression/', download_view.start_pdf_compression_and_show_download_view,
-         name='start_pdf_compression_and_show_download_view'),
-    path('start_png_compression/', download_view.start_png_compression_and_show_download_view,
-         name='start_png_compression_and_show_download_view'),
+    path('start_processing_and_show_download_view/', download_view.start_processing_and_show_download_view,
+         name='start_processing_and_show_download_view'),
     path('api/', include(api_urls))
 ]
 
