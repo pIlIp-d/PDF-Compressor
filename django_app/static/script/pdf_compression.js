07a09c1414eab71f814e232167295031d2fb5f18
pilip-d <philipdell@gmx.de>
2022-10-09 02:38:08 +0200

    applied new form render changes to js script

--- django_app/static/script/pdf_compression.js
+++ django_app/static/script/pdf_compression.js
@@ -4,11 +4,13 @@ function init() {
     // add onchange functions for special form elements which deactivate other options when checked
     let simple_and_lossless = document.getElementsByName("simple_and_lossless")[0];
     simple_and_lossless.onchange = function () {
-        update_visibility_of_container("not_lossless_container", this.checked);
+        let list_of_items = ["compression_mode", "default_pdf_dpi", "ocr_mode", "tesseract_language"]
+        for (let i in list_of_items)
+            update_visibility_of_container("id_" + list_of_items[i], this.checked);
     }
     let ocr_mode = document.getElementsByName("ocr_mode")[0];
     ocr_mode.onchange = function () {
-        update_visibility_of_container("no_ocr_container", this.value === "off");
+        update_visibility_of_container("id_tesseract_language", this.value === "off");
     };
     // call them once to initiate css classes from default values
     simple_and_lossless.onchange();
