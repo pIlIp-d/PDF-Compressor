d05e7745846441061057876abacf60b06d7070e6
pilip-d <philipdell@gmx.de>
2022-10-09 23:42:21 +0200

    moved, created and implemented classes for first Plugin showcase

---
+++ plugins/pdf_compressor/plugin_config.py
@@ -0,0 +1,18 @@
+from django_app.plugin_system.plugin import Plugin
+
+
+class PdfCompressorPlugin(Plugin):
+    def __init__(self, name: str):
+        super().__init__(
+            name=name,
+            from_file_types=["pdf", "png"],
+            form="plugins.pdf_compressor.pdf_compressor_form.PdfCompressorForm",
+            task="plugins.pdf_compressor.pdf_compression_task.PdfCompressionTask",
+            processor_class="pdfcompressor.pdfcompressor.PDFCrunchCompressor"
+        )
+
+    def get_destination_types(self, from_file_type: str = None) -> list[str]:
+        result = super().get_destination_types(from_file_type)
+        if from_file_type == "pdf" or from_file_type == "png":
+            result.append(self.COMPRESSION_TYPE)
+        return result
