6946ddc80770e675db39603c4bc88c10850da3f5
pilip-d <philipdell@gmx.de>
2022-10-09 23:32:31 +0200

    changed structure of tasks for new plugin system

--- django_app/task_scheduler/tasks/processing_task.py
+++ django_app/task_scheduler/tasks/processing_task.py
@@ -1,34 +1,32 @@
-from functools import reduce
-import os
 from abc import ABC
 from django_app.task_scheduler.tasks.task import Task
 from django_app.webserver.custom_models.process_stats_event_handler import ProcessStatsEventHandler
 from django_app.webserver.custom_models.zip_all_files_event_handler import ZipAllFilesEventHandler
+from django_app.webserver.models import ProcessingFilesRequest
 
 
 class ProcessingTask(Task, ABC):
-
-    def __init__(self, request_id: int, amount_of_input_files: int, processed_file_paths: list,
-                 task_id: int = None, finished: bool = False,
-                 **parameters):
-        parameters["amount_of_input_files"] = amount_of_input_files
-        parameters["processed_file_paths"] = processed_file_paths
-        super().__init__(request_id, task_id, finished, **parameters)
+    def __init__(
+            self,
+            processing_request: ProcessingFilesRequest,
+            processed_file_paths: list,
+            task_id: int = None,
+            finished: bool = False
+    ):
+        super().__init__(processing_request.id, task_id, finished)
+        self.__amount_of_input_files = len(
+            ProcessingFilesRequest.get_uploaded_file_list_of_current_request(processing_request)
+        )
+        self.__processed_file_paths = processed_file_paths
 
     def _get_process_stats_event_handler(self) -> list[ProcessStatsEventHandler]:
-        # remove extra args from parameters
-        amount_of_input_files = self._parameters["amount_of_input_files"]
-        processed_file_paths = self._parameters["processed_file_paths"]
-        self._parameters = {key: value for key, value in self._parameters.items() if
-                            key not in ("amount_of_input_files", "processed_file_paths")}
-
         return [
             ZipAllFilesEventHandler(
-                processed_file_paths[0]
+                self.__processed_file_paths[0]
             ),
             ProcessStatsEventHandler(
-                amount_of_input_files,
-                processed_file_paths,
-                self.request_id
+                self.__amount_of_input_files,
+                self.__processed_file_paths,
+                self._request_id
             )
         ]
