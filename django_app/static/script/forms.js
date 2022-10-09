d5e85933eae2c5c5faf31c9d6573012148a8c014
pilip-d <philipdell@gmx.de>
2022-10-09 23:35:24 +0200

    updated form.js to new container logic

--- django_app/static/script/forms.js
+++ django_app/static/script/forms.js
@@ -1,9 +1,12 @@
 function update_visibility_of_container(container_id, disabled) {
-    let container = document.getElementById(container_id);
-    let DISABLED_CLASS = "disabled_option";
-    if (disabled) {
-        container.classList.add(DISABLED_CLASS);
-    } else {
-        container.classList.remove(DISABLED_CLASS);
+    let containers = document.getElementsByClassName(container_id);
+    let DISABLED_CLASS = "disabled_container";
+    for (let i = 0; i < containers.length; i++) {
+        let container = containers[i];
+        if (disabled) {
+            container.classList.add(DISABLED_CLASS);
+        } else {
+            container.classList.remove(DISABLED_CLASS);
+        }
     }
 }
