79a3827c4468908b114ca01d121c53ec3697baf0
pilip-d <philipdell@gmx.de>
2022-10-09 02:36:45 +0200

    added custom form renderer -> removed extra form files

--- django_app/static/script/forms.js
+++ django_app/static/script/forms.js
@@ -1,6 +1,6 @@
 function update_visibility_of_container(container_id, disabled) {
     let container = document.getElementById(container_id);
-    let DISABLED_CLASS = "disabled_container";
+    let DISABLED_CLASS = "disabled_option";
     if (disabled) {
         container.classList.add(DISABLED_CLASS);
     } else {
