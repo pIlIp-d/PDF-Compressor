58931841767069b755642933a5d4f0e47c77bd45
pilip-d <philipdell@gmx.de>
2022-10-10 21:30:19 +0200

    moved pdfcompressor plugin_settings to separate folder

--- plugins/pdfcompressor/plugin_config/forms.py
+++ plugins/pdfcompressor/plugin_config/forms.py
@@ -87,3 +87,25 @@ class PdfCompressorForm(PluginForm):
         }
         # TODO document/implement a combi type
         # TODO document/implement an option to have multiple hierarchies per field
+
+
+class PngCompressorForm(PluginForm):
+    processing_file_extension = forms.RegexField(
+        regex="",  # TODO regex for allowed chars
+        initial="compressed",
+        label='Processing File Extension:',
+        help_text='TODO'
+    )
+    compression_mode = forms.TypedChoiceField(
+        choices=(
+            (1, '1: Extreme'),
+            (2, '2: Strong'),
+            (3, '3: Medium'),
+            (4, '4: Fast'),
+            (5, '5: Normal'),
+        ),
+        initial=5,
+        label='Compression mode:',
+        coerce=str,
+        help_text='TODO'
+    )
---
+++ plugins/pdfcompressor/plugin_config/plugin_config.py
@@ -0,0 +1,33 @@
+from django_app.plugin_system.plugin import Plugin
+
+
+class PdfCompressorPlugin(Plugin):
+    def __init__(self, name: str):
+        super().__init__(
+            name=name,
+            from_file_types=["pdf"],
+            form="plugins.pdfcompressor.plugin_config.pdf_compressor_form.PdfCompressorForm",
+            task="plugins.pdfcompressor.plugin_config.pdf_compression_task.PdfCompressionTask"
+        )
+
+    def get_destination_types(self, from_file_type: str = None) -> list[str]:
+        result = super().get_destination_types(from_file_type)
+        if from_file_type == "pdf":
+            result.append(self.COMPRESSION_TYPE)
+        return result
+
+
+class PngCompressorPlugin(Plugin):
+    def __init__(self, name: str):
+        super().__init__(
+            name=name,
+            from_file_types=["png"],
+            form="plugins.pdfcompressor.plugin_config.pdf_compressor_form.PngCompressorForm",
+            task="plugins.pdfcompressor.plugin_config.pdf_compression_task.PngCompressionTask"
+        )
+
+    def get_destination_types(self, from_file_type: str = None) -> list[str]:
+        result = super().get_destination_types(from_file_type)
+        if from_file_type == "png":
+            result.append(self.COMPRESSION_TYPE)
+        return result
