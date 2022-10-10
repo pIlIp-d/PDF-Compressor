cfd7a2e7b62a7ee4c8a7f4732783cc102ecd22f4
pilip-d <philipdell@gmx.de>
2022-10-10 23:29:48 +0200

    fixed typo

--- django_app/wsgi.py
+++ django_app/wsgi.py
@@ -11,10 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from django_app.task_scheduler.task_scheduler import TaskSchedulerDeamon
+from django_app.task_scheduler.task_scheduler import TaskSchedulerDaemon
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
 
 application = get_wsgi_application()
 
-TaskSchedulerDeamon.start_async()
+TaskSchedulerDaemon.start_async()
