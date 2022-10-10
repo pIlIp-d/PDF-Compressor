3389167bbc69fce2171d9e0bc7aaa8a64ee0491f
pilip-d <philipdell@gmx.de>
2022-10-10 22:39:20 +0200

    removed prints

--- django_app/api/decorators.py
+++ django_app/api/decorators.py
@@ -5,7 +5,6 @@ from django.http import JsonResponse
 def only_for_localhost(func):
     def run_decorated_func(*args, **kwargs):
         request = args[0]
-        print(request.META.get("REMOTE_ADDR"))
         if isinstance(request, WSGIRequest) and request.META.get("REMOTE_ADDR") == "127.0.0.1":
             return func(*args, **kwargs)
         else:
