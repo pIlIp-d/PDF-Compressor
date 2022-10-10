e6eecd4a2b89171d679569c9517b2618290ef968
pilip-d <philipdell@gmx.de>
2022-10-10 22:16:24 +0200

    added default and, changed get_hierarchy() signature

--- django_app/plugin_system/plugin_form.py
+++ django_app/plugin_system/plugin_form.py
@@ -1,6 +1,13 @@
-from django.forms import Form
+from django.forms import Form, RegexField
 
 
 class PluginForm(Form):
-    def get_hierarchy(self) -> list:
-        return []
+    processing_file_extension = RegexField(
+        regex="",  # TODO regex for allowed chars
+        initial="compressed",
+        label='Processing File Extension:',
+        help_text='TODO'
+    )
+
+    def get_hierarchy(self) -> dict:
+        return {}
