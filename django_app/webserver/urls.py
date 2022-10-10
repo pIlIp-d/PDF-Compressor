dd96b9c3ed1616f75143ce61baaea6df2b1fd1c8
pilip-d <philipdell@gmx.de>
2022-10-10 21:15:28 +0200

    moved webserver.views back to a single file

--- django_app/webserver/urls.py
+++ django_app/webserver/urls.py
@@ -2,16 +2,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import main_view, download_view
-from ..api.urls import urlpatterns as api_urls
 from . import views
+from ..api.urls import urlpatterns as api_urls
 
 app_name = 'webserver'
 
 urlpatterns = [
-    path('', main_view.render_main_view, name='main-view'),
-    path('download/', download_view.render_download_view, name='download_view'),
-    path('start_processing_and_show_download_view/', download_view.start_processing_and_show_download_view,
+    path('', views.render_main_view, name='main-view'),
+    path('download/', views.render_download_view, name='download_view'),
+    path('start_processing_and_show_download_view/', views.start_processing_and_show_download_view,
          name='start_processing_and_show_download_view'),
     path('api/', include(api_urls))
 ]
