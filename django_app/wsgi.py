0279c5eef5e8ecc2c2ce016bcd8c42b1b780420a
pilip-d <philipdell@gmx.de>
2022-10-12 01:26:40 +0200

    added plugin import validation

--- django_app/wsgi.py
+++ django_app/wsgi.py
@@ -11,10 +11,20 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from django_app import settings
 from django_app.task_scheduler.task_scheduler import TaskSchedulerDaemon
+from plugins.crunch_compressor.utility.console_utility import ConsoleUtility
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
 
 application = get_wsgi_application()
 
 TaskSchedulerDaemon.start_async()
+
+# validate plugin imports
+try:
+    for plugin in settings.PROCESSOR_PLUGINS:
+        plugin.get_form_class()
+        plugin.get_task()
+except ImportError as e:
+    ConsoleUtility.print_error(str(e))
