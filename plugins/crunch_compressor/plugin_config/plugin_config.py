354e3c7af8606d32e89a8dea991e2f99ace570a2
pilip-d <philipdell@gmx.de>
2022-10-13 00:58:00 +0200

    refactored

--- plugins/crunch_compressor/plugin_config/plugin_config.py
+++ plugins/crunch_compressor/plugin_config/plugin_config.py
@@ -6,10 +6,10 @@ class PdfCompressorPlugin(Plugin):
 
     def __init__(self, name: str):
         super().__init__(
-            name=name,
-            from_file_types=[self.PDF_MIME_TYPE],
-            form="plugins.crunch_compressor.plugin_config.forms.PdfCompressorForm",
-            task="plugins.crunch_compressor.plugin_config.tasks.PdfCompressionTask"
+            name,
+            [self.PDF_MIME_TYPE],
+            "plugins.crunch_compressor.plugin_config.forms.PdfCompressorForm",
+            "plugins.crunch_compressor.plugin_config.tasks.PdfCompressionTask"
         )
 
     def get_destination_types(self, from_file_type: str = None) -> list[str]:
@@ -23,10 +23,10 @@ class PdfCompressorPlugin(Plugin):
 class PngCompressorPlugin(Plugin):
     def __init__(self, name: str):
         super().__init__(
-            name=name,
-            from_file_types=["image/png"],
-            form="plugins.crunch_compressor.plugin_config.forms.PngCompressorForm",
-            task="plugins.crunch_compressor.plugin_config.tasks.PngCompressionTask"
+            name,
+            ["image/png"],
+            "plugins.crunch_compressor.plugin_config.forms.PngCompressorForm",
+            "plugins.crunch_compressor.plugin_config.tasks.PngCompressionTask"
         )
 
     def get_destination_types(self, from_file_type: str = None) -> list[str]:
@@ -39,8 +39,8 @@ class PngCompressorPlugin(Plugin):
 class ImageToPdfConvertPlugin(Plugin):
     def __init__(self, name: str):
         super().__init__(
-            name=name,
-            from_file_types=[
+            name,
+            [
                 "image/jpeg",
                 "image/pjpeg",
                 "image/png",
@@ -48,8 +48,8 @@ class ImageToPdfConvertPlugin(Plugin):
                 "image/svg+xml",
                 "image/webp",
             ],
-            form="plugins.crunch_compressor.plugin_config.forms.ImageToPdfConvertForm",
-            task="plugins.crunch_compressor.plugin_config.tasks.ImageToPdfConvertTask"
+            "plugins.crunch_compressor.plugin_config.forms.ImageToPdfConvertForm",
+            "plugins.crunch_compressor.plugin_config.tasks.ImageToPdfConvertTask"
         )
 
     def get_destination_types(self, from_file_type: str = None) -> list[str]:
