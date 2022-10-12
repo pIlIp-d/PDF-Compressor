1b48901d5a9fbfe25966cf0929a82f1ae621305c
pilip-d <philipdell@gmx.de>
2022-10-13 00:54:50 +0200

    fixed wrong variable usage

--- plugins/crunch_compressor/compressor/pdf_compressor/abstract_pdf_compressor.py
+++ plugins/crunch_compressor/compressor/pdf_compressor/abstract_pdf_compressor.py
@@ -17,10 +17,10 @@ class AbstractPdfCompressor(Compressor, ABC):
     def _merge_files(self, file_list: list[str], merged_result_file: str) -> None:
         merger = fitz.open()
         for file in file_list:
-            if os.path.exists(self._final_merge_file):
-                with fitz.open(self._final_merge_file) as f:
+            if os.path.exists(merged_result_file):
+                with fitz.open(merged_result_file) as f:
                     merger.insert_pdf(f)
             with fitz.open(file) as f:
                 merger.insert_pdf(f)
             os.remove(file)
-        merger.save(self._final_merge_file)
+        merger.save(merged_result_file)
