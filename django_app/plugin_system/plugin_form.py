171adeebbf9b0d547c1956831664cbef11e15422
pilip-d <philipdell@gmx.de>
2022-10-13 00:58:20 +0200

    removed unnecessary form field

--- django_app/plugin_system/plugin_form.py
+++ django_app/plugin_system/plugin_form.py
@@ -2,12 +2,6 @@ from django.forms import Form, RegexField
 
 
 class PluginForm(Form):
-    processing_file_extension = RegexField(
-        regex="",  # TODO regex for allowed chars
-        initial="processed",
-        label='Processing File Extension:',
-        help_text='TODO'
-    )
 
     def get_hierarchy(self) -> dict:
         return {}
