e4bf4636d7379549c33c19760d786858f579bce1
pilip-d <philipdell@gmx.de>
2022-10-14 01:51:38 +0200

    simplified and added code which was previously inside start_processing view

--- django_app/task_scheduler/event_handler/process_stats_event_handler.py
+++ django_app/task_scheduler/event_handler/process_stats_event_handler.py
@@ -1,48 +1,41 @@
-from time import sleep
+import datetime
+import os
 
-import jsons
-import requests
-
-from manage import ADDRESS, PORT, METHOD
+from django_app.task_scheduler.tasks.zip_task import ZipTask
+from django_app.webserver.models.processed_file import ProcessedFile
+from django_app.webserver.models.processing_files_request import ProcessingFilesRequest
+from django_app.webserver.string_utility import StringUtility
 from plugins.crunch_compressor.utility.EventHandler import EventHandler
 from plugins.crunch_compressor.utility.os_utility import OsUtility
 
 
+# TODO rename to ProcesEventHandler
 class ProcessStatsEventHandler(EventHandler):
-    def __init__(self, amount_of_input_files: int, result_files: list, request_id: int):
+    def __init__(self, request_id: int):
         super().__init__()
-        self.__amount_of_input_files = amount_of_input_files
-        self.__result_files = result_files
         self.__request_id = request_id
-        self.__finished_files = 0
-        self.__is_merger = len(result_files) == 1 and amount_of_input_files > 1
-
-    def get_progress(self):
-        return self.__finished_files / self.__amount_of_input_files
-
-    @staticmethod
-    def __make_request(request_string: str, args):
-        for i in range(3):  # retry 3 times on failure
-            status = requests.get(f"{METHOD}://{ADDRESS}:{PORT}/api" + request_string, args)
-            if status != 200:
-                sleep(1)
-            else:
-                return jsons.loads(status.text)
 
     def started_processing(self):
-        self.__make_request("/started_request_processing", {"request_id": str(self.__request_id)})
-
-    def postprocess(self, source_file: str, destination_file: str) -> None:
-        self.__finished_files += 1
-        if not self.__is_merger:
-            for file in self.__result_files[1:]:
-                if OsUtility.get_filename(destination_file) == OsUtility.get_filename(
-                        str(file.processed_file_path)) + "_temp":
-                    self.__make_request("/finish_file", {"processed_file_path": file.processed_file_path})
+        processing_request = ProcessingFilesRequest.get_request_by_id(self.__request_id)
+        processing_request.started = True
+        processing_request.save()
 
     def finished_all_files(self):
-        if self.__is_merger:
-            # event: merged file is finished
-            self.__make_request("/finish_file", {"processed_file_path": self.__result_files[1].processed_file_path})
-        # event: all files are finished
-        self.__make_request("/finish_request", {"request_id": str(self.__request_id)})
+        processing_request = ProcessingFilesRequest.get_request_by_id(self.__request_id)
+        processing_request.finished = True
+        processing_request.save()
+
+        folder = StringUtility.get_local_absolute_path(
+            os.path.join("uploaded_files", processing_request.user_id, str(processing_request.id) + "_processed")
+        )
+        zip_path = os.path.join(
+            folder, "processed_files_" + StringUtility.get_formatted_time(datetime.datetime.now()) + ".zip"
+        )
+        os.makedirs(folder, exist_ok=True)
+
+        # run synchronize
+        ZipTask(folder, zip_path).run()
+
+        # add files to download view
+        for file in reversed(OsUtility.get_file_list(folder)):
+            ProcessedFile.add_processed_file(file, processing_request)
