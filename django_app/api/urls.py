a2d7bb492b4f4b4ff97735bb971348220f787c0c
pilip-d <philipdell@gmx.de>
2022-10-14 01:47:12 +0200

    removed old views

--- django_app/api/urls.py
+++ django_app/api/urls.py
@@ -6,9 +6,6 @@ app_name = 'api'
 urlpatterns = [
     path('upload_file/', views.upload_file, name='upload-file'),
     path('remove_file/', views.remove_file, name='remove-file'),
-    path('started_request_processing/', views.started_request_processing, name='started_request_processing'),
-    path('finish_file/', views.finish_file, name='finish-file'),
-    path('finish_request/', views.finish_request, name='finish-request'),
     path('get_all_files/', views.get_all_files, name="get-all-files"),
     path('get_possible_destination_file_types/', views.get_possible_destination_file_types),
     path('get_form_html_for_web_view/', views.get_form_html_for_web_view)
