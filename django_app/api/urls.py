d80ea1ba4dd9a7035cb22af27c4553282922c44f
pilip-d <philipdell@gmx.de>
2022-10-18 11:08:49 +0200

    moved access from task_scheduler thread (EventHandler class) to views by using requests

--- django_app/api/urls.py
+++ django_app/api/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('remove_file/', views.remove_file, name='remove-file'),
     path('get_all_files/', views.get_all_files, name="get-all-files"),
     path('get_possible_destination_file_types/', views.get_possible_destination_file_types),
+    path('finished_all_files/', views.finished_all_files),
+    path('started_processing/', views.started_processing),
     path('get_form_html_for_web_view/', views.get_form_html_for_web_view)
 ]
