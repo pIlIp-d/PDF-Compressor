6b45d682b7237b32ceffabb183a9ded61efc3471
pilip-d <philipdell@gmx.de>
2022-10-18 11:08:49 +0200

    moved access from task_scheduler thread (EventHandler class) to views by using requests

--- django_app/api/views.py
+++ django_app/api/views.py
@@ -1,16 +1,57 @@
+import datetime
+import os
+
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_protect
 from django_app import settings
 from django_app.plugin_system.plugin import Plugin
-from django_app.webserver.models.processed_file import ProcessedFile
-from django_app.webserver.models.processing_files_request import ProcessingFilesRequest
 from django_app.webserver.models.uploaded_file import UploadedFile
 from django_app.webserver.validators import get_file_extension
+from django_app.task_scheduler.tasks.zip_task import ZipTask
+from django_app.webserver.models.processed_file import ProcessedFile
+from django_app.webserver.models.processing_files_request import ProcessingFilesRequest
+from django_app.webserver.string_utility import StringUtility
+from django_app.utility.os_utility import OsUtility
 
 
 # TODO favicon.ico
 # TODO move valid_file_endings from UploadedFile -> request Table
 # TODO decorator for method type
+
+
+def started_processing(request):
+    if request.method != "GET":
+        return wrong_method_error("GET")
+    processing_request = ProcessingFilesRequest.get_request_by_id(request.GET.get("request_id"))
+    processing_request.started = True
+    processing_request.save()
+    return JsonResponse({"status": 200})
+
+
+def finished_all_files(request):
+    if request.method != "GET":
+        return wrong_method_error("GET")
+    processing_request = ProcessingFilesRequest.get_request_by_id(request.GET.get("request_id"))
+    processing_request.finished = True
+    processing_request.save()
+
+    folder = StringUtility.get_local_absolute_path(
+        os.path.join("uploaded_files", processing_request.user_id, str(processing_request.id) + "_processed")
+    )
+    zip_path = os.path.join(
+        folder, "processed_files_" + StringUtility.get_formatted_time(datetime.datetime.now()) + ".zip"
+    )
+    os.makedirs(folder, exist_ok=True)
+
+    # run synchronize
+    ZipTask(folder, zip_path).run()
+
+    # add files to download view
+    for file in reversed(OsUtility.get_file_list(folder)):
+        ProcessedFile.add_processed_file(StringUtility.get_media_normalized_path(file), processing_request)
+
+    return JsonResponse({"status": 200})
+
 
 @csrf_protect
 def get_all_files(request):
--- django_app/task_scheduler/event_handler/process_stats_event_handler.py
+++ django_app/task_scheduler/event_handler/process_stats_event_handler.py
@@ -1,12 +1,7 @@
-import datetime
-import os
+import requests
 
-from django_app.task_scheduler.tasks.zip_task import ZipTask
-from django_app.webserver.models.processed_file import ProcessedFile
-from django_app.webserver.models.processing_files_request import ProcessingFilesRequest
-from django_app.webserver.string_utility import StringUtility
+import manage
 from django_app.utility.event_handler import EventHandler
-from django_app.utility.os_utility import OsUtility
 
 
 # TODO rename to ProcesEventHandler
@@ -16,26 +11,13 @@ class ProcessStatsEventHandler(EventHandler):
         self.__request_id = request_id
 
     def started_processing(self):
-        processing_request = ProcessingFilesRequest.get_request_by_id(self.__request_id)
-        processing_request.started = True
-        processing_request.save()
+        self.__make_request("api/started_processing/")
 
     def finished_all_files(self):
-        processing_request = ProcessingFilesRequest.get_request_by_id(self.__request_id)
-        processing_request.finished = True
-        processing_request.save()
+        self.__make_request("api/finished_all_files/")
 
-        folder = StringUtility.get_local_absolute_path(
-            os.path.join("uploaded_files", processing_request.user_id, str(processing_request.id) + "_processed")
-        )
-        zip_path = os.path.join(
-            folder, "processed_files_" + StringUtility.get_formatted_time(datetime.datetime.now()) + ".zip"
-        )
-        os.makedirs(folder, exist_ok=True)
-
-        # run synchronize
-        ZipTask(folder, zip_path).run()
-
-        # add files to download view
-        for file in reversed(OsUtility.get_file_list(folder)):
-            ProcessedFile.add_processed_file(StringUtility.get_media_normalized_path(file), processing_request)
+    def __make_request(self, path):
+        status = requests.get(f"{manage.METHOD}://localhost:{manage.PORT}/" + path,
+                              {"request_id": self.__request_id})
+        if status.status_code != 200:
+            raise ConnectionError(status)
