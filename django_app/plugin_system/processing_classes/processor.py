40bfc41ceff7354ffaef45d287e9066a2243bd80
pilip-d <philipdell@gmx.de>
2022-10-23 15:55:05 +0200

    added Python DOCs, refactored

--- django_app/plugin_system/processing_classes/processor.py
+++ django_app/plugin_system/processing_classes/processor.py
@@ -11,6 +11,7 @@ from django_app.plugin_system.processing_classes.event_handler import EventHandl
 from django_app.utility.console_utility import ConsoleUtility
 from django_app.utility.os_utility import OsUtility
 
+
 # TODO multi-threaded load balancing of available threads
 # TODO eventHandler list isn't Threadsafe (when multiple processing requests are executed on the same Processor object)
 
@@ -23,8 +24,16 @@ class Processor(Postprocessor, Preprocessor, ABC):
             file_type_to: str,
             can_merge: bool = False,
             run_multi_threaded: bool = True,
-            processed_part: str = "All Files"
     ):
+        """
+        abstract class that simplifies file processing by only implementing process_file in subclasses
+        :param event_handlers: list of EventHandlers of which the events are called in every stage of Processing
+        :param file_type_from: source_files type TODO mime type
+        :param file_type_to: type of result/processed files TODO mime type
+        :param can_merge: enables the _merge_files() method after processing all files<br>
+                -> _merge_files() must be implemented in subclass
+        :param run_multi_threaded: allow to call process_file() for multiple files at the same time (multi threaded)
+        """
         if event_handlers is None:
             event_handlers = list()
         self._event_handlers = event_handlers
@@ -32,7 +41,6 @@ class Processor(Postprocessor, Preprocessor, ABC):
         self._postprocessors = []
         self.__add_event_handler_processors()
         self._processed_files_appendix = "_processed"
-        self._processed_part = processed_part
         self._can_merge = can_merge
         self._file_type_from = file_type_from.lower()
         self._file_type_to = file_type_to.lower()
@@ -66,54 +74,80 @@ class Processor(Postprocessor, Preprocessor, ABC):
 
     @abstractmethod
     def process_file(self, source_file: str, destination_path: str) -> None:
+        """
+        process a source_file end create file/files in destination_path
+        :param source_file: path to a file that is processed
+        :param destination_path: path to a file or directory depending on class you are extending
+        """
         pass
 
     def _process_file_list(self, source_files: list, destination_files: list) -> None:
+        """
+        sequential processing of files in source_files to destination_files<br>
+        length of source_files and destination_files needs to be equal
+        """
+        if len(source_files) != len(destination_files):
+            raise ValueError("length of 'source_files' and 'destination_files' needs to be equal.")
         for source_file, destination_file in zip(source_files, destination_files):
             self.process_file(source_file, destination_file)
 
     def process_file_list_multi_threaded(
             self,
-            source_files: list,
-            destination_files: list,
+            source_files: list[str],
+            destination_files: list[str],
             cpu_count: int = os.cpu_count()
     ) -> None:
+        """
+        parallel processing of files in source_files to destination_files<br>
+        length of source_files and destination_files needs to be equal
+        :param source_files: list of source files
+        :param destination_files: list of destination files/directories depending on the implementation of superclass
+        :param cpu_count: cpu cores on which the process_file calls are distributed to
+        """
         args_list = [{"source_file": source, "destination_path": destination}
                      for source, destination in zip(source_files, destination_files)]
         self._custom_map_execute(self.process_file, args_list, cpu_count)
 
-    def _get_temp_folder(self):
+    def _get_and_create_temp_folder(self) -> str:
+        """
+        creates and returns a unique folder path inside the project directory (used for internal file storing)
+        """
         while True:
             counter = uuid.uuid4()
-            temp_file = os.path.abspath(
-                os.path.join("", "temporary_files", str(counter) + self._processed_files_appendix))
-            if not os.path.isdir(temp_file):
-                return temp_file
+            temp_folder = os.path.abspath(
+                os.path.join("", "temporary_files", str(counter) + self._processed_files_appendix)
+            )
+            if not os.path.isdir(temp_folder):
+                os.makedirs(temp_folder)
+                return temp_folder
 
-    def _get_temp_destination(self, temp_folder, file_path: str):
-        if self._destination_path_string_is_file(file_path):
+    def _get_temp_destination(self, temp_folder, destination_path: str):
+        """
+        merges temp_folder and destination_path to get a temporary destination path
+        """
+        if self._destination_path_string_is_file(destination_path):
             return os.path.abspath(
-                os.path.join(temp_folder, OsUtility.get_filename(file_path) + "." + self._file_type_to)
+                os.path.join(temp_folder, OsUtility.get_filename(destination_path) + "." + self._file_type_to)
             )
         else:
             return os.path.abspath(
-                os.path.join(temp_folder, os.path.basename(file_path))
+                os.path.join(temp_folder, os.path.basename(destination_path))
             )
 
     def _merge_files(self, file_list: list[str], merged_result_file: str) -> None:
         """
-        Method needs to be implemented if 'can_merge' is set to True.
+        Method needs to be implemented if 'can_merge' is set to True.<br>
         It is called in self.process() if there are more than one source_file and just one destination_file
         :param file_list: files that are merged and removed afterwards
         :param merged_result_file: result file containing a merged version of the file_list
         """
         raise ValueError("If 'can_merge' is set to True the class must provide a _merge_files implementation.")
 
-    def _destination_path_string_is_file(self, path):
+    def _destination_path_string_is_file(self, path) -> bool:
         return path.endswith(self._file_type_to)
 
-    def _get_files_and_extra_info_from_input_folder(self, source_path, destination_path
-                                                    ) -> tuple[list[str], list[str], bool, bool]:
+    def __get_files_and_extra_info_from_input_folder(self, source_path, destination_path
+                                                     ) -> tuple[list[str], list[str], bool, bool]:
         sources = OsUtility.get_file_list(source_path, self._file_type_from)
         merging = len(sources) > 1 and self._destination_path_string_is_file(destination_path)
         output_folder = source_path + self._processed_files_appendix if destination_path == "default" else destination_path
@@ -126,8 +160,8 @@ class Processor(Postprocessor, Preprocessor, ABC):
         ]
         return sources, destinations, merging, False
 
-    def _get_files_and_extra_info_from_input_file(self, source_path, destination_path
-                                                  ) -> tuple[list[str], list[str], bool, bool]:
+    def __get_files_and_extra_info_from_input_file(self, source_path, destination_path
+                                                   ) -> tuple[list[str], list[str], bool, bool]:
         input_path_without_file_ending = OsUtility.get_path_without_file_ending(source_path)
 
         if destination_path == "default":
@@ -157,25 +191,31 @@ class Processor(Postprocessor, Preprocessor, ABC):
         folder -> default: same as file -> folder, folder name <br>
         file -> default: merges into default a file in the directory of the source file merge_#timestring#.xxx<br>
         """
-        return self._get_files_and_extra_info_from_input_folder(source_path, destination_path) if os.path.isdir(
-            source_path) else self._get_files_and_extra_info_from_input_file(source_path, destination_path)
+        return self.__get_files_and_extra_info_from_input_folder(source_path, destination_path) if os.path.isdir(
+            source_path) else self.__get_files_and_extra_info_from_input_file(source_path, destination_path)
 
     def process(self, source_path, destination_path):
+        """
+        implements advanced checks, behaviour, optimization for process_file<br>
+        supports different situations and improves compatibility
+        :param source_path: either path to a file or a folder
+        :param destination_path: path to a file, a folder, "default" or "merge"
+        specific behavior is defined by _get_files_and_extra_info() and can be changed by overwriting the method
+        """
         if not os.path.exists(source_path):
             raise ValueError("No File or Folder was found at the given source_path.")
 
+        # get usable lists of source, destination files
         force_merge = destination_path == "merge"
         if destination_path == "merge":
             source_dir = source_path if os.path.isdir(source_path) else os.path.dirname(source_path)
             destination_path = os.path.join(
                 source_dir, "merged_" + StringUtility.get_formatted_time(datetime.now()) + "." + self._file_type_to
             )
-
         source_file_list, destination_path_list, is_merging, is_splitting = self._get_files_and_extra_info(
             source_path,
             destination_path
         )
-
         is_merging = force_merge or is_merging
 
         if len(source_file_list) == 0:
@@ -185,17 +225,17 @@ class Processor(Postprocessor, Preprocessor, ABC):
         orig_sizes = OsUtility.get_filesize_list(source_file_list)
 
         # create temporary destinations to avoid data loss
-        temp_folder = self._get_temp_folder()
+        temp_folder = self._get_and_create_temp_folder()
         temporary_destination_file_list = [
             self._get_temp_destination(temp_folder, path)
             for path in destination_path_list
         ]
 
-        final_merge_file = None
+        temporary_merge_file = None
         if is_merging:
             if not self._can_merge:
                 raise ValueError("Merging is not supported for this Processor. " + str(self))
-            final_merge_file = OsUtility.get_path_without_file_ending(
+            temporary_merge_file = OsUtility.get_path_without_file_ending(
                 temporary_destination_file_list[0]) + "_merged." + self._file_type_to
 
         for event_handler in self._event_handlers:
@@ -209,22 +249,22 @@ class Processor(Postprocessor, Preprocessor, ABC):
 
         if is_merging:
             # move merge result to destination
-            self._merge_files(temporary_destination_file_list, final_merge_file)
+            self._merge_files(temporary_destination_file_list, temporary_merge_file)
             ConsoleUtility.print_green(
-                f"Merged {self._processed_part} into {ConsoleUtility.get_file_string(final_merge_file)}"
+                f"Merged All Files into {ConsoleUtility.get_file_string(temporary_merge_file)}"
             )
 
         if is_merging:
-            end_size = OsUtility.get_file_size(final_merge_file)
+            end_size = OsUtility.get_file_size(temporary_merge_file)
         else:
             end_size = sum(OsUtility.get_filesize_list(temporary_destination_file_list))
         # TODO move all (non-error) prints out of this class
         if len(source_file_list) > 1:
-            ConsoleUtility.print_stats(sum(orig_sizes), end_size, self._processed_part)
+            ConsoleUtility.print_stats(sum(orig_sizes), end_size, "All Files")
             ConsoleUtility.print("\n")
 
         if is_merging and not is_splitting:
-            OsUtility.move_file(final_merge_file, destination_path)
+            OsUtility.move_file(temporary_merge_file, destination_path)
         elif is_splitting:
             for file in OsUtility.get_file_list(temporary_destination_file_list[0]):
                 OsUtility.move_file(file, os.path.join(destination_path_list[0], os.path.basename(file)))
