515264074f88ea9c3c2206d508cd83f1cca1f1c6
pilip-d <philipdell@gmx.de>
2022-10-23 19:05:53 +0200

    renamed tasks.db -> tasks.sqlite3

--- django_app/settings.py
+++ django_app/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'django_app' / 'db.sqlite3',
+        'NAME': BASE_DIR / 'django_app' / 'models.sqlite3',
         'OPTIONS': {
             'timeout': 2,  # in seconds
             # see also
--- django_app/task_scheduler/db_con.py
+++ django_app/task_scheduler/db_con.py
@@ -3,6 +3,6 @@ import sqlite3
 
 
 def get_connection():
-    conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), "tasks.db"))
+    conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), "tasks.sqlite3"))
     conn.row_factory = sqlite3.Row
     return conn
