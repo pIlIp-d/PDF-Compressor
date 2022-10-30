4b36703ad39f3b861d82fc20ff28be7b0ef1c1a5
pilip-d <philipdell@gmx.de>
2022-10-30 19:07:40 +0100

    added TestProcessorConstructor with test stubs

---
+++ tests/test_processor_constructor.py
@@ -0,0 +1,65 @@
+import multiprocessing
+from unittest import TestCase
+
+
+manager = multiprocessing.Manager()
+ns = manager.Namespace()
+lock = manager.Lock()
+
+
+class TestProcessorConstructor(TestCase):
+    def test_processor_with_file_from_type_is_empty_list(self):
+        self.fail("not implemented yet")
+
+    def test_processor_with_file_from_type_is_list_of_empty_string(self):
+        self.fail("not implemented yet")
+
+    def test_processor_with_file_from_type_is_single_valid_value(self):
+        self.fail("not implemented yet")
+
+    def test_processor_with_file_from_type_are_multiple_valid_values(self):
+        self.fail("not implemented yet")
+
+    def test_processor_with_file_from_type_is_single_invalid_value(self):
+        self.fail("not implemented yet")
+
+    def test_processor_with_file_from_type_are_multiple_invalid_values(self):
+        self.fail("not implemented yet")
+
+    def test_processor_with_file_from_type_are_mixed_valid_and_invalid_values(self):
+        self.fail("not implemented yet")
+
+    def test_processor_with_file_from_type_are_duplicate_valid_values(self):
+        self.fail("not implemented yet")
+
+    def test_processor_with_file_from_type_and_single_file_found(self):
+        self.fail("not implemented yet")
+
+    def test_processor_with_file_from_type_and_no_file_found(self):
+        self.fail("not implemented yet")
+
+    def test_postprocess_file_type_to_is_empty_string(self):
+        self.fail("not implemented yet")
+
+    def test_postprocess_file_type_to_is_invalid_string(self):
+        self.fail("not implemented yet")
+
+    def test_postprocess_file_type_to_is_valid_string(self):
+        self.fail("not implemented yet")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
