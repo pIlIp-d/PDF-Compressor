4df53a083ec3347c3a54138073d6f019aaa5ce8f
pilip-d <philipdell@gmx.de>
2022-10-30 21:02:59 +0100

    moved utility classes to help_classes.py

---
+++ tests/help_classes.py
@@ -0,0 +1,51 @@
+import shutil
+
+from django_app.plugin_system.processing_classes.event_handler import EventHandler
+from django_app.plugin_system.processing_classes.processor import Processor
+from django_app.plugin_system.processing_classes.processorwithdestinationfolder import ProcessorWithDestinationFolder
+
+
+class SimpleExampleProcessor(Processor):
+    def __init__(self, event_handler: [EventHandler]):
+        super().__init__(event_handler, ["txt"], "txt")
+
+    def process_file(self, source_file: str, destination_path: str) -> None:
+        self.preprocess(source_file, destination_path)
+        shutil.copyfile(source_file, destination_path)
+        self.postprocess(source_file, destination_path)
+
+
+class SimpleProcessorForFileTypes(Processor):
+    def __init__(self, file_type_from: list[str], file_type_to: str):
+        super().__init__([], file_type_from, file_type_to)
+
+    def process_file(self, source_file: str, destination_path: str) -> None:
+        self.preprocess(source_file, destination_path)
+        if destination_path.endswith("pdf"):
+            raise ValueError("can't convert to that file type")
+        shutil.copyfile(source_file, destination_path)
+        self.postprocess(source_file, destination_path)
+
+
+class FailedProcessingException(Exception):
+    pass
+
+
+class ErrorProcessor(Processor):
+    def __init__(self, event_handlers: list[EventHandler]):
+        super().__init__(event_handlers, ["txt"], "txt")
+
+    def process_file(self, source_file: str, destination_path: str) -> None:
+        self.preprocess(source_file, destination_path)
+        raise FailedProcessingException
+        self.postprocess(source_file, destination_path)
+
+
+class DestinationFolderSubClass(ProcessorWithDestinationFolder):
+    def __init__(self, event_handlers: list[EventHandler]):
+        super().__init__(event_handlers, ["txt"], "txt")
+
+    def process_file(self, source_file: str, destination_path: str) -> None:
+        self.preprocess(source_file, destination_path)
+        shutil.copyfile(source_file, destination_path)
+        self.postprocess(source_file, destination_path)
