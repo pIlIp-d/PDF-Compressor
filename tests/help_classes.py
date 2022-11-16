cb50f77b1eaa37c907ce56fc446d2eaac2948931
pilip-d <philipdell@gmx.de>
2022-11-16 22:12:46 +0100

    refactored and added merge for new testcases

--- tests/help_classes.py
+++ tests/help_classes.py
@@ -5,17 +5,32 @@ import time
 from django_app.plugin_system.processing_classes.event_handler import EventHandler
 from django_app.plugin_system.processing_classes.processor import Processor
 from django_app.plugin_system.processing_classes.processorwithdestinationfolder import ProcessorWithDestinationFolder
-from django_app.utility.os_utility import OsUtility
+
+
+def simple_copy_process_file(self, source_file: str, destination_path: str) -> None:
+    self.preprocess(source_file, destination_path)
+    shutil.copyfile(source_file, destination_path)
+    self.postprocess(source_file, destination_path)
+
+
+def simple_merge_files(self, file_list: list[str], merged_result_file: str) -> None:
+    with open(merged_result_file, "w") as result_file:
+        for file in file_list:
+            with open(file) as f:
+                result_file.write(f.read())
 
 
 class SimpleExampleProcessor(Processor):
-    def __init__(self, event_handler: [EventHandler]):
-        super().__init__(event_handler, ["txt"], "txt")
+    def __init__(self, event_handlers=None, **kwargs):
+        if event_handlers is None:
+            event_handler = []
+        super().__init__(event_handlers, ["txt"], "txt", **kwargs)
 
     def process_file(self, source_file: str, destination_path: str) -> None:
-        self.preprocess(source_file, destination_path)
-        shutil.copyfile(source_file, destination_path)
-        self.postprocess(source_file, destination_path)
+        simple_copy_process_file(self, source_file, destination_path)
+
+    def _merge_files(self, file_list: list[str], merged_result_file: str) -> None:
+        simple_merge_files(self, file_list, merged_result_file)
 
 
 class SimpleProcessorForFileTypes(Processor):
@@ -23,11 +38,9 @@ class SimpleProcessorForFileTypes(Processor):
         super().__init__([], file_type_from, file_type_to)
 
     def process_file(self, source_file: str, destination_path: str) -> None:
-        self.preprocess(source_file, destination_path)
         if destination_path.endswith("pdf"):
             raise ValueError("can't convert to that file type")
-        shutil.copyfile(source_file, destination_path)
-        self.postprocess(source_file, destination_path)
+        simple_copy_process_file(self, source_file, destination_path)
 
 
 class SimpleFakeMergeProcessor(Processor):
@@ -36,18 +49,13 @@ class SimpleFakeMergeProcessor(Processor):
         super().__init__([], ["txt"], "txt", can_merge=can_merge)
 
     def process_file(self, source_file: str, destination_path: str) -> None:
-        self.preprocess(source_file, destination_path)
-        shutil.copyfile(source_file, destination_path)
-        self.postprocess(source_file, destination_path)
+        simple_copy_process_file(self, source_file, destination_path)
 
 
 class SimpleMergeProcessor(SimpleFakeMergeProcessor):
     # used for testing can_merge and implements the _merge_files method
     def _merge_files(self, file_list: list[str], merged_result_file: str) -> None:
-        with open(merged_result_file, "w") as result_file:
-            for file in file_list:
-                with open(file) as f:
-                    result_file.write(f.read())
+        simple_merge_files(self, file_list, merged_result_file)
 
 
 class FailedProcessingException(Exception):
@@ -65,13 +73,16 @@ class ErrorProcessor(Processor):
 
 
 class DestinationFolderSubClass(ProcessorWithDestinationFolder):
-    def __init__(self, event_handlers: list[EventHandler]):
-        super().__init__(event_handlers, ["txt"], "txt")
+    def __init__(self, event_handlers, **kwargs):
+        if event_handlers is None:
+            event_handlers = []
+        super().__init__(event_handlers, ["txt"], "txt", **kwargs)
 
     def process_file(self, source_file: str, destination_path: str) -> None:
-        self.preprocess(source_file, destination_path)
-        shutil.copy(source_file, destination_path)
-        self.postprocess(source_file, destination_path)
+        simple_copy_process_file(self, source_file, os.path.join(destination_path, os.path.basename(source_file)))
+
+    def _merge_files(self, file_list: list[str], merged_result_file: str) -> None:
+        simple_merge_files(self, file_list, merged_result_file)
 
 
 class SimpleMultiThreadTestProcessor(Processor):
