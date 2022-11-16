d04b166f2765af8815af6cba17af4b4d61197b65
pilip-d <philipdell@gmx.de>
2022-11-16 21:51:39 +0100

    improved cleanup

--- tests/test_processor_run_multi_threaded.py
+++ tests/test_processor_run_multi_threaded.py
@@ -10,21 +10,21 @@ from tests.test_processor import ns as test_processor_ns
 
 
 class TestProcessorConstructorRunMultiThreaded(TestCase):
+    source_folder = os.path.join(".", "TestData", "manyFiles")
 
     def __run_processing_for_many_files(self, multi_threaded: bool) -> float:
         # create Test Data
-        source_folder = os.path.join(".", "TestData", "manyFiles")
-        if os.path.isdir(source_folder):
-            shutil.rmtree(source_folder)
-        os.makedirs(source_folder)
+        if os.path.isdir(self.source_folder):
+            shutil.rmtree(self.source_folder)
+        os.makedirs(self.source_folder)
         for i in range(100):
-            with open(os.path.join(source_folder, str(i) + ".txt"), "w+") as f:
+            with open(os.path.join(self.source_folder, str(i) + ".txt"), "w+") as f:
                 f.write(str(i))
         destination_folder = os.path.join(".", "TestData", "outputFiles")
         start_time = time.time()
         SimpleMultiThreadTestProcessor(
             [TestingEventHandler()], multi_threaded
-        ).process(source_folder, destination_folder)
+        ).process(self.source_folder, destination_folder)
         self.assertEqual(100, test_processor_ns.amount_of_postprocess_calls)
         self.assertEqual(100, len(OsUtility.get_file_list(destination_folder)))
         elapsed_time = time.time() - start_time
@@ -43,4 +43,5 @@ class TestProcessorConstructorRunMultiThreaded(TestCase):
 
     @classmethod
     def tearDownClass(cls) -> None:
+        shutil.rmtree(cls.source_folder)
         clean_up_after_class()
