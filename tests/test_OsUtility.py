b16e07e49e7f3482e5d185fa21c38e0a85db874c
pilip-d <philipdell@gmx.de>
2022-11-16 22:47:37 +0100

    moved OsUtility test to new test folder

--- tests/test_OsUtility.py
+++ tests/test_OsUtility.py
@@ -9,7 +9,7 @@ from plugins.crunch_compressor.config import get_config
 
 
 class TestOsUtility(TestCase):
-    CONFIG_FILE: str = os.path.abspath(os.path.join("..", "..", "config.json"))
+    CONFIG_FILE: str = os.path.abspath(os.path.join("../plugins/crunch_compressor", "..", "config.json"))
 
     @staticmethod
     def create_file(file_path: str) -> None:
@@ -24,14 +24,14 @@ class TestOsUtility(TestCase):
 
     # get_file_list
     def test_get_file_list_from_empty_folder(self):
-        folder_dir = os.path.join("", "TestData", "emptyFolder")
+        folder_dir = os.path.join("", "../plugins/crunch_compressor/tests/TestData", "emptyFolder")
         self.create_folder(folder_dir)
         file_list = OsUtility.get_file_list(folder_dir)
         shutil.rmtree(folder_dir)
         self.assertEqual(0, len(file_list))
 
     def test_get_file_list_from_single_file_in_folder(self):
-        folder_dir = os.path.join("", "TestData", "singleFileFolder")
+        folder_dir = os.path.join("", "../plugins/crunch_compressor/tests/TestData", "singleFileFolder")
         self.create_folder(folder_dir)
 
         file = os.path.join(folder_dir, "testFile.png")
@@ -44,7 +44,7 @@ class TestOsUtility(TestCase):
         self.assertEqual(file, file_list[0])
 
     def test_get_file_list_from_multiple_files_in_folder(self):
-        folder_dir = os.path.join("", "TestData", "singleFileFolder")
+        folder_dir = os.path.join("", "../plugins/crunch_compressor/tests/TestData", "singleFileFolder")
         self.create_folder(folder_dir)
 
         file1 = os.path.join(folder_dir, "testFile1.png")
@@ -65,13 +65,13 @@ class TestOsUtility(TestCase):
     def test_get_file_list_invalid_path(self):
         self.assertRaises(
             FileNotFoundError,
-            OsUtility.get_file_list, os.path.join("", "TestData", "moreFilesFolder")
+            OsUtility.get_file_list, os.path.join("", "../plugins/crunch_compressor/tests/TestData", "moreFilesFolder")
         )
 
     def test_get_file_list_from_not_existing_folder(self):
         self.assertRaises(
             FileNotFoundError,
-            OsUtility.get_file_list, os.path.join("", "TestData", "notExistingFolder")
+            OsUtility.get_file_list, os.path.join("", "../plugins/crunch_compressor/tests/TestData", "notExistingFolder")
         )
 
     def test_get_file_list_with_empty_string_as_folder(self):
@@ -81,7 +81,7 @@ class TestOsUtility(TestCase):
         )
 
     def test_get_file_list_with_relative_folder_path(self):
-        folder_dir = os.path.join("", "TestData", "singleFileFolder")
+        folder_dir = os.path.join("", "../plugins/crunch_compressor/tests/TestData", "singleFileFolder")
         self.create_folder(folder_dir)
 
         file = os.path.join(folder_dir, "testFile.png")
@@ -93,7 +93,7 @@ class TestOsUtility(TestCase):
         self.assertEqual(1, len(file_list))
 
     def test_get_file_list_with_absolute_folder_path(self):
-        folder_dir = os.path.abspath(os.path.join("", "TestData", "singleFileFolder"))
+        folder_dir = os.path.abspath(os.path.join("", "../plugins/crunch_compressor/tests/TestData", "singleFileFolder"))
         self.create_folder(folder_dir)
 
         file = os.path.join(folder_dir, "testFile.png")
@@ -107,11 +107,11 @@ class TestOsUtility(TestCase):
     def test_get_file_list_with_file_as_folder_path(self):
         self.assertRaises(
             ValueError,
-            OsUtility.get_file_list, os.path.join("", "TestData", "singlePagePdf.pdf")
+            OsUtility.get_file_list, os.path.join("", "../plugins/crunch_compressor/tests/TestData", "singlePagePdf.pdf")
         )
 
     def test_get_file_list_with_empty_ending(self):
-        folder_dir = os.path.join("", "TestData", "moreFilesFolder")
+        folder_dir = os.path.join("", "../plugins/crunch_compressor/tests/TestData", "moreFilesFolder")
         self.create_folder(folder_dir)
 
         file1 = os.path.join(folder_dir, "testFile1.png")
@@ -130,7 +130,7 @@ class TestOsUtility(TestCase):
         self.assertTrue(file3 in file_list)
 
     def test_get_file_list_with_custom_ending(self):
-        folder_dir = os.path.abspath(os.path.join("", "TestData", "moreFilesFolder"))
+        folder_dir = os.path.abspath(os.path.join("", "../plugins/crunch_compressor/tests/TestData", "moreFilesFolder"))
         self.create_folder(folder_dir)
 
         file1 = os.path.join(folder_dir, "testFile1.png")
@@ -147,7 +147,7 @@ class TestOsUtility(TestCase):
         self.assertTrue(file3 in file_list)
 
     def test_get_file_list_with_dot_as_ending(self):
-        folder_dir = os.path.join("", "TestData", "moreFilesFolder")
+        folder_dir = os.path.join("", "../plugins/crunch_compressor/tests/TestData", "moreFilesFolder")
         self.create_folder(folder_dir)
 
         file1 = os.path.join(folder_dir, "testFile1.png")
@@ -171,7 +171,7 @@ class TestOsUtility(TestCase):
         shutil.rmtree(folder_dir)
 
     def test_get_file_with_text_as_ending(self):
-        folder_dir = os.path.join("", "TestData", "moreFilesFolder")
+        folder_dir = os.path.join("", "../plugins/crunch_compressor/tests/TestData", "moreFilesFolder")
         self.create_folder(folder_dir)
 
         file1 = os.path.join(folder_dir, "testFile1.png")
@@ -193,7 +193,7 @@ class TestOsUtility(TestCase):
 
     # clean_up_folder
     def test_clean_up_folder_with_empty_folder(self):
-        folder_dir = os.path.join("", "TestData", "emptyFolder")
+        folder_dir = os.path.join("", "../plugins/crunch_compressor/tests/TestData", "emptyFolder")
         self.create_folder(folder_dir)
 
         OsUtility.clean_up_folder(folder_dir)
@@ -201,7 +201,7 @@ class TestOsUtility(TestCase):
         self.assertFalse(os.path.exists(folder_dir))
 
     def test_clean_up_folder_with_files_in_folder(self):
-        folder_dir = os.path.join("", "TestData", "filledFolder")
+        folder_dir = os.path.join("", "../plugins/crunch_compressor/tests/TestData", "filledFolder")
         self.create_folder(folder_dir)
 
         self.create_file(os.path.join(folder_dir, "testFile1.png"))
@@ -218,7 +218,7 @@ class TestOsUtility(TestCase):
         )
 
     def test_clean_up_folder_with_relative_path_to_existing_file(self):
-        file = os.path.join("", "TestData", "fileToDelete.png")
+        file = os.path.join("", "../plugins/crunch_compressor/tests/TestData", "fileToDelete.png")
         self.create_file(file)
         self.assertRaises(
             ValueError,
@@ -227,7 +227,7 @@ class TestOsUtility(TestCase):
         os.remove(file)
 
     def test_clean_up_folder_with_absolute_path_to_existing_file(self):
-        file = os.path.join("", "TestData", "fileToDelete.png")
+        file = os.path.join("", "../plugins/crunch_compressor/tests/TestData", "fileToDelete.png")
         self.create_file(file)
         self.assertRaises(
             ValueError,
@@ -236,14 +236,14 @@ class TestOsUtility(TestCase):
         os.remove(file)
 
     def test_clean_up_folder_with_relative_path_to_existing_folder(self):
-        folder_dir = os.path.abspath(os.path.join("", "TestData", "existingFolder"))
+        folder_dir = os.path.abspath(os.path.join("", "../plugins/crunch_compressor/tests/TestData", "existingFolder"))
         self.create_folder(folder_dir)
 
         OsUtility.clean_up_folder(folder_dir)
         self.assertFalse(os.path.exists(folder_dir))
 
     def test_clean_up_folder_with_absolute_path_to_existing_folder(self):
-        folder_dir = os.path.join("", "TestData", "existingFolder")
+        folder_dir = os.path.join("", "../plugins/crunch_compressor/tests/TestData", "existingFolder")
         self.create_folder(folder_dir)
 
         OsUtility.clean_up_folder(folder_dir)
@@ -265,19 +265,19 @@ class TestOsUtility(TestCase):
         self.assertEqual("singlePagePdf", OsUtility.get_filename(file))
 
     def test_get_filename_with_absolute_path_to_file(self):
-        file = os.path.join(os.path.abspath("TestData/singlePagePdf.pdf"))
+        file = os.path.join(os.path.abspath("../plugins/crunch_compressor/tests/TestData/singlePagePdf.pdf"))
         self.assertEqual("singlePagePdf", OsUtility.get_filename(file))
 
     def test_get_filename_with_relative_path_to_folder(self):
-        file = os.path.join("TestData/TestFolder")
+        file = os.path.join("../plugins/crunch_compressor/tests/TestData/TestFolder")
         self.assertEqual("TestFolder", OsUtility.get_filename(file))
 
     def test_get_filename_with_absolute_path_to_folder(self):
-        file = os.path.join(os.path.abspath(os.path.join("", "TestData", "TestFolder")))
+        file = os.path.join(os.path.abspath(os.path.join("", "../plugins/crunch_compressor/tests/TestData", "TestFolder")))
         self.assertEqual("TestFolder", OsUtility.get_filename(file))
 
     def test_get_filename_with_invalid_path(self):
-        file = os.path.join(os.path.abspath("TestData/singlePagePdf.pdf"))
+        file = os.path.join(os.path.abspath("../plugins/crunch_compressor/tests/TestData/singlePagePdf.pdf"))
         self.assertEqual("singlePagePdf", OsUtility.get_filename(file))
 
         file2 = os.path.join(os.path.abspath("SomeRubbishTest-LALA-File_folder(jjkn)"))
@@ -326,38 +326,38 @@ class TestOsUtility(TestCase):
         self.assertEqual(0, OsUtility.get_file_size(os.path.join("", "file_not_exists.pdf")))
 
     def test_os_utility_get_file_size_relative_path(self):
-        self.assertNotEqual(0, OsUtility.get_file_size(os.path.join("", "TestData", "singlePagePdf.pdf")))
+        self.assertNotEqual(0, OsUtility.get_file_size(os.path.join("", "../plugins/crunch_compressor/tests/TestData", "singlePagePdf.pdf")))
 
     def test_os_utility_get_file_size_absolute_path(self):
         self.assertNotEqual(0, OsUtility.get_file_size(
-            os.path.abspath(os.path.join("", "TestData", "singlePagePdf.pdf")))
+            os.path.abspath(os.path.join("", "../plugins/crunch_compressor/tests/TestData", "singlePagePdf.pdf")))
         )
 
     def test_os_utility_get_file_size_compare_results(self):
         self.assertTrue(
-            OsUtility.get_file_size(os.path.join("", "TestData", "singlePagePdf.pdf"))
+            OsUtility.get_file_size(os.path.join("", "../plugins/crunch_compressor/tests/TestData", "singlePagePdf.pdf"))
             <
-            OsUtility.get_file_size(os.path.join("", "TestData", "multiPageTestData.pdf"))
+            OsUtility.get_file_size(os.path.join("", "../plugins/crunch_compressor/tests/TestData", "multiPageTestData.pdf"))
         )
 
     def test_os_utility_get_file_size_on_filled_folder(self):
-        self.assertEqual(0, OsUtility.get_file_size(os.path.join("", "TestData")))
+        self.assertEqual(0, OsUtility.get_file_size(os.path.join("", "../plugins/crunch_compressor/tests/TestData")))
 
     def test_os_utility_get_file_size_on_empty_folder(self):
-        empty_dir = os.path.join("", "TestData", "emptyFolder")
+        empty_dir = os.path.join("", "../plugins/crunch_compressor/tests/TestData", "emptyFolder")
         if not os.path.isdir(empty_dir):
             os.mkdir(empty_dir)
         self.assertEqual(0, OsUtility.get_file_size(empty_dir))
         os.removedirs(empty_dir)
 
     def test_os_utility_get_file_size_on_not_existing_folder(self):
-        empty_dir = os.path.join("", "TestData", "not_existing_folder")
+        empty_dir = os.path.join("", "../plugins/crunch_compressor/tests/TestData", "not_existing_folder")
         if os.path.isdir(empty_dir):
             os.removedirs(empty_dir)
         self.assertEqual(0, OsUtility.get_file_size(empty_dir))
 
     def test_get_path_without_file_ending(self):
-        file_without = os.path.join(".", "directory", "file")
+        file_without = os.path.join("../plugins/crunch_compressor/tests", "directory", "file")
         file_with = file_without + ".txt"
         self.assertEqual(file_without, OsUtility.get_path_without_file_ending(file_with))
 
