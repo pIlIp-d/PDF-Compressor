a77fec5dcb0fc6bc71192cce23627d88c77f6888
pilip-d <philipdell@gmx.de>
2022-11-18 00:48:38 +0100

    refactored

--- django_app/utility/os_utility.py
+++ django_app/utility/os_utility.py
@@ -1,4 +1,3 @@
-import mimetypes
 import os
 import re
 import shutil
@@ -8,17 +7,17 @@ from django_app.utility.console_utility import ConsoleUtility
 
 class OsUtility:
     @classmethod
-    def get_file_list(cls, folder: str, ending: str = "") -> list:
+    def get_file_list(cls, file: str, ending: str = "") -> list:
         def valid_filename(filename):
             return filename.lower().endswith(ending.lower())
 
-        if not os.path.exists(folder):
+        if not os.path.exists(file):
             return []
-        if os.path.isfile(folder):
-            return [folder] if valid_filename(folder) else []
+        if os.path.isfile(file):
+            return [file] if valid_filename(file) else []
         # get all the png files in temporary folder <=> all pdf pages
         files = []
-        for r, _, f in os.walk(folder):
+        for r, _, f in os.walk(file):
             for file_name in f:
                 if not valid_filename(file_name):
                     continue
