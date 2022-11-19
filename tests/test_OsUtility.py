40497c86e9b5c0a08f9a276214d4895ce13aab8f
pilip-d <philipdell@gmx.de>
2022-11-19 12:52:32 +0100

    moved config test to corresponding plugin folder

--- tests/test_OsUtility.py
+++ tests/test_OsUtility.py
@@ -2,15 +2,11 @@ import os.path
 import shutil
 from unittest import TestCase
 
-import jsons
-
 from django_app.utility.os_utility import OsUtility
-from plugins.crunch_compressor.config import get_config
 from tests.help_classes import TESTDATA_DIR
 
 
 class TestOsUtility(TestCase):
-    CONFIG_FILE: str = os.path.abspath(os.path.join("../plugins/crunch_compressor", "..", "config.json"))
 
     @staticmethod
     def create_file(file_path: str) -> None:
@@ -184,36 +180,6 @@ class TestOsUtility(TestCase):
     def test_get_filename_with_custom_file_ending_path(self):
         self.assertEqual("filename", OsUtility.get_filename("filename.old.pdf", r"\..+\..+"))
 
-    def test_os_utility_get_config_file_not_found(self):
-        shutil.move(self.CONFIG_FILE, self.CONFIG_FILE + ".tmp")
-        self.assertRaises(
-            FileNotFoundError,
-            get_config,
-            self.CONFIG_FILE
-        )
-        # move tmp file back
-        shutil.move(self.CONFIG_FILE + ".tmp", self.CONFIG_FILE)
-
-    def test_os_utility_get_config_all_values_set(self):
-        # no error -> all must have been set
-        get_config(self.CONFIG_FILE)
-
-    def test_os_utility_get_config_not_all_values_are_set(self):
-        shutil.copyfile(self.CONFIG_FILE, self.CONFIG_FILE + ".tmp")
-
-        with open(self.CONFIG_FILE, "r") as config_file:
-            json = jsons.loads(config_file.read())
-
-        with open(self.CONFIG_FILE, "w") as config_file:
-            json.pop("cpdfsqueeze_path", None)
-            config_file.write(jsons.dumps(json))
-        self.assertRaises(
-            TypeError,
-            get_config,
-            self.CONFIG_FILE
-        )
-        shutil.move(self.CONFIG_FILE + ".tmp", self.CONFIG_FILE)
-
     # file_size
     def test_os_utility_get_file_size_file_doesnt_exist(self):
         self.assertEqual(0, OsUtility.get_file_size(os.path.join("", "noFile.txt")))
@@ -249,9 +215,3 @@ class TestOsUtility(TestCase):
         file_without = os.path.join("directory", "file")
         file_with = file_without + ".txt"
         self.assertEqual(file_without, OsUtility.get_path_without_file_ending(file_with))
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        # reset if get_config() test has failed
-        if os.path.exists(cls.CONFIG_FILE + ".tmp"):
-            shutil.move(cls.CONFIG_FILE + ".tmp", cls.CONFIG_FILE)
