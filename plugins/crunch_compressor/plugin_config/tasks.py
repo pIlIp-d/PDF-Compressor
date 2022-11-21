4384a068b6bd0ccb4d6308c75e063bfe8835ee21
pilip-d <philipdell@gmx.de>
2022-11-21 22:33:28 +0100

    changed to new version of pdf_compressor

--- plugins/crunch_compressor/plugin_config/tasks.py
+++ plugins/crunch_compressor/plugin_config/tasks.py
@@ -1,29 +1,28 @@
 import mimetypes
 
-from django_app import settings
 from django_app.task_scheduler.tasks.processing_task import ProcessingTask
 from plugins.crunch_compressor.compressor.converter.images_to_pdf_converter import ImagesToPdfConverter
 from plugins.crunch_compressor.compressor.converter.pdf_to_image_converter import PdfToImageConverter
+from plugins.crunch_compressor.compressor.pdf_compressor.pdf_crunch_compressor import PDFCrunchCompressor
 from plugins.crunch_compressor.compressor.png_compressor.png_crunch_compressor import PNGCrunchCompressor
 from plugins.crunch_compressor.config import get_config
-from plugins.crunch_compressor.pdfcompressor import PDFCompressor
 
 
 class PdfCompressionTask(ProcessingTask):
     def run(self):
         event_handler = super()._get_event_handler()
-        PDFCompressor(
-            source_path=self._source_path,
-            destination_path=self._destination_path,
+        PDFCrunchCompressor(
             compression_mode=int(self._request_parameters.get("compression_mode")),
             force_ocr=self._request_parameters.get("ocr_mode") == "on",
             no_ocr=self._request_parameters.get("ocr_mode") == "off",
-            quiet=not settings.DEBUG,
             tesseract_language=self._request_parameters.get("tesseract_language"),
             simple_and_lossless=self._request_parameters.get("simple_and_lossless") == "on",
             default_pdf_dpi=int(self._request_parameters.get("default_pdf_dpi")),
             event_handlers=event_handler
-        ).compress()
+        ).process(
+            self._source_path,
+            self._destination_path
+        )
 
 
 class PngCompressionTask(ProcessingTask):
