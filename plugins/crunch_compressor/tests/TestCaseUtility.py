1c0892a3ca0b380f4c053be5d30ee2707b8654c0
pilip-d <philipdell@gmx.de>
2022-11-21 21:44:20 +0100

    removed old class

--- plugins/crunch_compressor/tests/TestCaseUtility.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-
-def run_subprocess_and_get_output(args: list) -> tuple[int, str, str]:
-    p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, err = p.communicate()
-    return p.returncode, str(output), str(err)
