b8d1a0e2b52d23598865e946c74e865ae107c5a5
pilip-d <philipdell@gmx.de>
2023-09-03 19:18:43 +0200

    fixed path for staticfiles

--- backend/django_app/settings.py
+++ backend/django_app/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / "django_app" / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'django_app' / 'media'
