72fe4c971112e2d4d60c2a109e1c33e4cf9eaa2e
pilip-d <philipdell@gmx.de>
2023-09-05 16:59:58 +0200

    removed old frontend from django

--- backend/django_app/static/script/upload_view/AllFilesRow.js
+++
@@ -1,6 +0,0 @@
-class AllFilesRow extends Row {
-    constructor(_current_plugin) {
-        super(null);
-        this.options.current_plugin = _current_plugin;
-    }
-}
--- backend/django_app/webserver/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from django_app.webserver.models.uploaded_file import UploadedFile
-
-# Register your models here.
-
-admin.site.register(UploadedFile)
-# TODO register the other models
